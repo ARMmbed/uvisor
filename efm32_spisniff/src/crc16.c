db61812437568c5bfe6a93427038e31c2744406f
Milosch Meriac <milosch.meriac@arm.com>
2014-01-13 11:02:59 +0000

    ruggedized CRC16

--- efm32_spisniff/src/crc16.c
+++ efm32_spisniff/src/crc16.c
@@ -2,17 +2,18 @@
 #include "crc16.h"
 
 uint16_t
-crc_ccitt (uint8_t * data, unsigned int length)
+crc_ccitt (uint8_t * data, uint32_t length)
 {
     uint16_t crc = 0xFFFF;
 
-    for (int j = 0; j < length; j++)
-        {
-            crc = (crc >> 8) | (crc << 8);
-            crc ^= data[j];
-            crc ^= (crc & 0xFF) >> 4;
-            crc ^= crc << 12;
-            crc ^= (crc & 0xFF) << 5;
-        }
-    return crc;
+    while(length--)
+    {
+        crc = (crc >> 8) | (crc << 8);
+        crc ^= *data++;
+        crc ^= (crc & 0xFF) >> 4;
+        crc ^= crc << 12;
+        crc ^= (crc & 0xFF) << 5;
+    }
+
+    return crc ^ 0xFFFF;
 }
--- efm32_spisniff/src/crc16.h
+++ efm32_spisniff/src/crc16.h
@@ -1,6 +1,6 @@
 #ifndef __CRC16_H__
 #define __CRC16_H__
 
-extern uint16_t crc_ccitt (uint8_t * data, unsigned int length);
+extern uint16_t crc_ccitt (uint8_t * data, uint32_t length);
 
 #endif/*__CRC16_H__*/
