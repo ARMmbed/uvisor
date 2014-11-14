bd02b3a2923fa4d0d0b84166029a273c9a935a79
Milosch Meriac <milosch.meriac@arm.com>
2014-11-14 13:15:10 +0000

    fixed bss section, added clean

--- core/linker/MK64FN1M0VLL12.h
+++ core/linker/MK64FN1M0VLL12.h
@@ -1,7 +1,7 @@
 #define FLASH_ORIGIN 0x00000000
 #define FLASH_LENGTH 0x100000
 
-#define SRAM_ORIGIN  0x20000000
+#define SRAM_ORIGIN  0x1FFF0000
 #define SRAM_LENGTH  0x20000
 
 #include "default.h"
