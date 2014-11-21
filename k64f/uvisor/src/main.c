0398aae60ad019b75dc54d87fd7181bffdcc4133
Milosch Meriac <milosch.meriac@arm.com>
2014-11-21 11:59:29 +0000

    removed intermediate mbed-code

--- k64f/uvisor/src/main.c
+++ k64f/uvisor/src/main.c
@@ -24,12 +24,6 @@ void main_entry(void)
     int t;
     volatile int i;
 
-    /* lr is supposed not to have changed in the reset_handler
-     * if this is not true the reset_handler must store lr and
-     * pass it as an argument here */
-    register uint32_t __lr asm("lr");
-    uint32_t lr = __lr;
-
     /* reset previous channel settings */
     ITM->LAR  = 0xC5ACCE55;
     ITM->TCR  = ITM->TER = 0x0;
@@ -46,13 +40,6 @@ void main_entry(void)
 
     /* init MPU */
     vmpu_init();
-
-    /* see comment above - assuming lr did not change in the
-     * reset_handler */
-    asm volatile(
-        "blx    %0\n"
-        :: "r" (lr)
-    );
 
     /* should never happen */
     while(1)
