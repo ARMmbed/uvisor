470da284958767d707f330d6ed6dddf7c0b22e5b
Milosch Meriac <milosch.meriac@arm.com>
2014-11-25 12:00:25 +0000

    linker symbols cleanup

--- core/system/inc/linker.h
+++ core/system/inc/linker.h
@@ -1,16 +1,13 @@
 #ifndef __LINKER_H__
 #define __LINKER_H__
 
-EXTERN uint32_t __stack_end__;
+EXTERN const uint32_t __stack_end__;
 
 EXTERN uint32_t __bss_start__;
 EXTERN uint32_t __bss_end__;
-EXTERN const uint32_t __bss_size__;
 
 EXTERN uint32_t __data_start__;
 EXTERN uint32_t __data_end__;
 EXTERN const uint32_t __data_start_src__;
-EXTERN const uint32_t __data_size__;
-EXTERN uint32_t __stack_start__;
-EXTERN const uint32_t __code_end__;
+
 #endif/*__LINKER_H__*/
