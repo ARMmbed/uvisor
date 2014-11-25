d44d4213efc5a034cf35c0841d3f521b3171c30a
Milosch Meriac <milosch.meriac@arm.com>
2014-11-25 10:28:32 +0000

    fixed sperr parsing

--- k64f/uvisor/src/vmpu.c
+++ k64f/uvisor/src/vmpu.c
@@ -21,7 +21,7 @@ static void mpu_fault(int reason)
                 t,
                 MPU->SP[t].EAR,
                 MPU->SP[t].EDR);
-        sperr >>= 1;
+        sperr <<= 1;
     }
     dprintf("CFSR : 0x%08X (reason 0x%02x)\n\r", SCB->CFSR, reason);
     while(1);
