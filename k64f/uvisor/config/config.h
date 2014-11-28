13a92cb6e5ff42311d2d3c01ae303221ab77e9bb
Alessandro Angelino <alessandro.angelino@arm.com>
2014-11-28 10:32:46 +0000

    added macros for NOSYSTEM defines

--- k64f/uvisor/config/config.h
+++ k64f/uvisor/config/config.h
@@ -5,14 +5,20 @@
 /* ensure  that SoC initialization is ignored */
 //#define NOSYSTEM
 
+#ifndef NOSYSTEM
 #define NV_CONFIG_OFFSET 0x400
+#endif/*NOSYSTEM*/
+
 #define CHANNEL_DEBUG 1
 #define XTAL_FREQUENCY 48000000UL
 
 #define USE_FLASH_SIZE UVISOR_FLASH_SIZE
 #define USE_SRAM_SIZE  UVISOR_SRAM_SIZE
-//#define RESERVED_FLASH 0x420
-//#define RESERVED_SRAM  0x220
+
+#ifdef  NOSYSTEM
+#define RESERVED_FLASH 0x420
+#define RESERVED_SRAM  0x220
+#endif/*NOSYSTEM*/
 
 #define STACK_SIZE 2048
 #define STACK_GUARD_BAND 128
