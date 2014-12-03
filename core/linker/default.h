c241b57a276ae8775a4feae50112f8448245c833
Milosch Meriac <milosch.meriac@arm.com>
2014-12-03 21:52:24 +0000

    allow enable/disable of uvisor

--- core/linker/default.h
+++ core/linker/default.h
@@ -65,6 +65,7 @@ SECTIONS
         KEEP(*(.box_init*))
         __box_init_end__ = .;
         PROVIDE(__data_start_src__ = LOADADDR(.data));
+        PROVIDE(__uvisor_config = LOADADDR(.data) + SIZEOF(.data));
         PROVIDE(__stack_end__ = ORIGIN(STACK) + LENGTH(STACK) - STACK_GUARD_BAND);
         . = ALIGN(16);
         __code_end__ = .;
--- core/system/inc/linker.h
+++ core/system/inc/linker.h
@@ -12,4 +12,13 @@ EXTERN uint32_t __data_start__;
 EXTERN uint32_t __data_end__;
 EXTERN const uint32_t __data_start_src__;
 
+typedef struct {
+    uint32_t magic;
+    uint32_t version;
+    uint32_t mode;
+    uint32_t *cfgtbl_start, *cfgtbl_end;
+} PACKED TUVisorConfig;
+
+EXTERN const TUVisorConfig __uvisor_config;
+
 #endif/*__LINKER_H__*/
