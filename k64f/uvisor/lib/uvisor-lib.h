79a1468596e95ad8f50f8783052d206250d249f2
Milosch Meriac <milosch.meriac@arm.com>
2014-12-06 16:00:47 +0000

    fixed configuration header file

--- k64f/uvisor/lib/uvisor-lib.h
+++ k64f/uvisor/lib/uvisor-lib.h
@@ -14,26 +14,27 @@
 #define UVISOR_SET_MODE(mode) UVISOR_EXTERN const uint32_t __uvisor_mode = (mode);
 #define UVISOR_ARRAY_COUNT(x) (sizeof(x)/sizeof(x[0]))
 #define UVISOR_ROUND32(x) (((x)+31UL) & ~0x1FUL)
+#define UVISOR_PAD32(x) (32 - (sizeof(x) & ~0x1FUL))
 
-#ifndef/*UVISOR_BOX_STACK*/
-#define  UVISOR_BOX_STACK 1024
-#endif /*UVISOR_BOX_STACK*/
+#ifndef UVISOR_BOX_STACK
+#define UVISOR_BOX_STACK 1024
+#endif/*UVISOR_BOX_STACK*/
 
-#define UVISOR_BOX_CONFIG(config_type) \
+#define UVISOR_SECURE(config_type) \
     __attribute__((section(".text.secured"), aligned(32))) \
     static const volatile struct { \
         config_type secure; \
-        uint8_t padding[UVISOR_ROUND32(config_type) - sizeof(config_type)]; \
-    } UVISOR_PACKED __uvisor_box_settings =
+        uint8_t padding[UVISOR_PAD32(config_type)]; \
+    } UVISOR_PACKED
 
-typedef union
-{
-    uint32_t value;
-} UVISOR_PACKED UvBoxAcl;
+#define UVISOR_BOX_CONFIG(box_config) \
+    UVISOR_EXTERN const __attribute__((section(".uvisor.cfgtbl"), aligned(4))) void* const __ptr_ ## box_config =  &box_config;
+
+typedef uint32_t UvBoxAcl;
 
 typedef struct
 {
-    const void* start;
+    const volatile void* start;
     uint32_t length;
     UvBoxAcl acl;
 } UVISOR_PACKED UvBoxAclItem;
@@ -46,10 +47,6 @@ typedef struct
 
     const UvBoxAclItem* const acl_list;
     uint32_t acl_count;
-
-    const void** const fn_list;
-    uint32_t fn_count;
-    uint32_t reserved;
 } UVISOR_PACKED UvBoxConfig;
 
 #endif/*__UVISOR_LIB_H__*/
