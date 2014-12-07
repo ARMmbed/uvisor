b6bbf353ca28027b72b665c954f08ac5120bab53
Milosch Meriac <milosch.meriac@arm.com>
2014-12-07 21:36:03 +0000

    deleted obsolete EFM32GG code

--- efm32/box/src/xor_wrap.h
+++
@@ -1,7 +0,0 @@
-#ifndef __XOR_WRAP_H__
-#define __XOR_WRAP_H__
-
-/* xor functions */
-uint32_t xor_enc(uint32_t);
-
-#endif /*__XOR_WRAP_H__*/
--- efm32/xor/src/timer_wrap.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIMER_WRAP_H__
-#define __TIMER_WRAP_H__
-
-/* timer functions */
-void timer_start(void);
-void timer_stop(void);
-
-#endif /*__TIMER_WRAP_H__*/
