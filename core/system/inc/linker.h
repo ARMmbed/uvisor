1245b63c9b606303dd5abe18673c1c1fc09a468c
Milosch Meriac <milosch.meriac@arm.com>
2014-12-07 12:35:17 +0000

    renamed section according to new linker script

--- core/system/inc/linker.h
+++ core/system/inc/linker.h
@@ -16,7 +16,16 @@ typedef struct {
     uint32_t magic;
     uint32_t version;
     uint32_t *mode;
+
+    /* initialized secret data section */
     uint32_t *cfgtbl_start, *cfgtbl_end;
+
+    /* initialized secret data section */
+    uint32_t *data_src, *data_start, *data_end;
+
+    /* uvisors protected flash memory regions */
+    uint32_t *secure_start, *secure_end;
+
 } PACKED TUVisorConfig;
 
 EXTERN const TUVisorConfig __uvisor_config;
