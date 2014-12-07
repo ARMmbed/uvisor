70bed752b36f172fd38b0fa9a8dafd92a89be251
Milosch Meriac <milosch.meriac@arm.com>
2014-12-07 21:36:03 +0000

    deleted obsolete EFM32GG code

--- efm32/spisniff/src/crc16.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CRC16_H__
-#define __CRC16_H__
-
-extern uint16_t crc_ccitt (const void* data, uint32_t length);
-
-#endif/*__CRC16_H__*/
--- efm32/uvisor/src/svc.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SVC_H__
-#define __SVC_H__
-
-/* svc initialization */
-extern void svc_init(void);
-
-#endif/*__SVC_H__*/
