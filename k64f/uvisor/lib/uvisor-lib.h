13abecd0d484c13145af19efdeb29e238044ce1b
Milosch Meriac <milosch.meriac@arm.com>
2014-12-15 11:24:49 +0000

    added license headers and LICENSE top-level file

--- k64f/uvisor/lib/uvisor-lib.h
+++ k64f/uvisor/lib/uvisor-lib.h
@@ -1,3 +1,15 @@
+/***************************************************************
+ * This confidential and  proprietary  software may be used only
+ * as authorised  by  a licensing  agreement  from  ARM  Limited
+ *
+ *             (C) COPYRIGHT 2013-2014 ARM Limited
+ *                      ALL RIGHTS RESERVED
+ *
+ *  The entire notice above must be reproduced on all authorised
+ *  copies and copies  may only be made to the  extent permitted
+ *  by a licensing agreement from ARM Limited.
+ *
+ ***************************************************************/
 #ifndef __UVISOR_LIB_H__
 #define __UVISOR_LIB_H__
 
