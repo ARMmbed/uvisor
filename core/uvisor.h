e08add5b1a5e104c4afb5b230b755f04f695f628
Alessandro Angelino <alessandro.angelino@arm.com>
2015-03-03 17:04:55 +0000

    Fixed vector table relocation bug

--- core/uvisor.h
+++ core/uvisor.h
@@ -42,8 +42,8 @@
 typedef void (*UnprivilegedBoxEntry)(void);
 
 #define NOINLINE __attribute__ ((noinline))
-#define PACKED __attribute__((packed))
-#define WEAK __attribute__ ((weak))
+#define PACKED   __attribute__((packed))
+#define WEAK     __attribute__ ((weak))
 #define ALIAS(f) __attribute__ ((weak, alias (#f)))
 
 /* array count macro */
@@ -60,7 +60,8 @@ typedef void (*UnprivilegedBoxEntry)(void);
 #define assert(...)
 #else /*NDEBUG*/
 #define DPRINTF dprintf
-#define assert(x) if(!(x)){dprintf("HALTED(%s:%i): assert(%s)\n",  __FILE__, __LINE__, #x);while(1);}
+#define assert(x) if(!(x)){dprintf("HALTED(%s:%i): assert(%s)\n",\
+                                   __FILE__, __LINE__, #x);while(1);}
 #endif/*NDEBUG*/
 
 #ifdef  CHANNEL_DEBUG
@@ -81,10 +82,6 @@ typedef void (*UnprivilegedBoxEntry)(void);
 
 /* IOT-OS specific includes */
 #include <isr.h>
-#include <svc-helper.h>
 #include <linker.h>
-
-/* library context switch utilities */
-#include <exports.h>
 
 #endif/*__UVISOR_H__*/
