2cba96cb2e9aa44e3eb2395b8f978479126b4174
Alessandro Angelino <alessandro.angelino@arm.com>
2015-03-16 13:57:56 +0100

    Added svc handlers to access unvic framework

--- k64f/uvisor/src/unvic.c
+++ k64f/uvisor/src/unvic.c
@@ -10,19 +10,64 @@
  *  by a licensing agreement from ARM Limited.
  *
  ***************************************************************/
-
 #include <uvisor.h>
 #include <isr.h>
 #include "halt.h"
 #include "unvic.h"
+#include "svc.h"
+
+/* unprivileged vector table */
+TIsrUVector g_unvic_vector[ISR_VECTORS];
 
 /* isr_default_handler to nvic_unpriv_default_handler */
-void isr_default_handler(void) UVISOR_LINKTO(nvic_unpriv_default_handler);
+void isr_default_handler(void) UVISOR_LINKTO(unvic_default_handler);
 
 /* unprivileged default handler */
-void nvic_unpriv_default_handler(void)
+void unvic_default_handler(void)
 {
     HALT_ERROR("spurious IRQ (IPSR=%i)", (uint8_t)__get_IPSR());
+}
+
+void unvic_isr_mux(void)
+{
+    return;
+}
+
+/* FIXME check if allowed (ACL) */
+void unvic_set_isr(uint32_t irqn, uint32_t vector, uint32_t flag)
+{
+    return;
+}
+
+uint32_t unvic_get_isr(uint32_t irqn)
+{
+    return 1;
+}
+
+void unvic_let_isr(uint32_t irqn)
+{
+    return;
+}
+
+/* FIXME check if allowed (ACL) */
+void unvic_ena_irq(uint32_t irqn)
+{
+    return;
+}
+
+void unvic_dis_irq(uint32_t irqn)
+{
+    return;
+}
+
+void unvic_set_ena_isr(uint32_t irqn, uint32_t vector, uint32_t flag)
+{
+    return;
+}
+
+void unvic_dis_let_isr(uint32_t isr)
+{
+    return;
 }
 
 void unvic_init(void)
