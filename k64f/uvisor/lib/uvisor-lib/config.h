de3a6932ef250cd1597ea1330b05ff9755600a72
Milosch Meriac <milosch.meriac@arm.com>
2015-03-27 04:57:56 +0000

    added main box - implemented stack, data, peripheral ACL decoding - updated default ACL's

--- k64f/uvisor/lib/uvisor-lib/config.h
+++ k64f/uvisor/lib/uvisor-lib/config.h
@@ -16,9 +16,26 @@
 UVISOR_EXTERN const uint32_t __uvisor_mode;
 
 #define UVISOR_SET_MODE(mode) \
+    UVISOR_SET_MODE_ACL_COUNT(mode, NULL, 0);
+
+#define UVISOR_SET_MODE_ACL(mode, acl_list) \
+    UVISOR_SET_MODE_ACL_COUNT(mode, acl_list, UVISOR_ARRAY_COUNT(acl_list));
+
+#define UVISOR_SET_MODE_ACL_COUNT(mode, acl_list, acl_list_count) \
     uint8_t __attribute__((section(".uvisor.bss.stack"), aligned(32))) \
         __reserved_stack[UVISOR_STACK_BAND_SIZE]; \
-    UVISOR_EXTERN const uint32_t __uvisor_mode = (mode);
+    UVISOR_EXTERN const uint32_t __uvisor_mode = (mode); \
+    \
+    static UVISOR_SECURE_CONST UvisorBoxConfig main_cfg = { \
+        UVISOR_BOX_MAGIC, \
+        UVISOR_BOX_VERSION, \
+        0, \
+        acl_list, \
+        acl_list_count \
+    }; \
+    \
+    const __attribute__((section(".uvisor.cfgtbl_first"), aligned(4))) \
+          volatile void* main_cfg_ptr  =  & main_cfg;
 
 #define UVISOR_SECURE_CONST \
     const volatile __attribute__((section(".uvisor.secure"), aligned(32)))
