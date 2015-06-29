00f5b8a38fdb3eb98797e7a48e472d4694bbc662
Alessandro Angelino <alessandro.angelino@arm.com>
2015-06-29 15:52:08 +0100

    Updated uvisor-lib header files to fit mbed style
    All headers are included with quotes, except for system-wide ones

--- k64f/uvisor/lib/source/benchmark.cpp
+++ k64f/uvisor/lib/source/benchmark.cpp
@@ -10,8 +10,8 @@
  *  by a licensing agreement from ARM Limited.
  *
  ***************************************************************/
-#include <mbed/mbed.h>
-#include <uvisor-lib/uvisor-lib.h>
+#include "mbed/mbed.h"
+#include "uvisor-lib/uvisor-lib.h"
 
 void uvisor_benchmark_configure(void)
 {
