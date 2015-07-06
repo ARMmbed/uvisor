0f4b3f000c17991f749d1ba13aa762cba1d701ad
Alessandro Angelino <alessandro.angelino@arm.com>
2015-07-06 16:34:13 +0100

    halt_led ported to STM32F429xx
    Halts the system with a pre-defined LED blinking pattern, giving hints on the
    reason for halting even when a debugger is not present

--- stm32f4/uvisor/src/stm32f4_halt.c
+++ stm32f4/uvisor/src/stm32f4_halt.c
@@ -13,6 +13,38 @@
 #include <uvisor.h>
 #include "halt.h"
 
+#define HALT_LED_PIN 14
+
 void halt_led(THaltError reason)
 {
+    uint32_t toggle;
+    volatile uint32_t delay;
+
+    int flag;
+
+    /* enable clock for port C */
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
+
+    /* enable output for pin */
+    GPIOG->MODER &= ~(0x3 << (HALT_LED_PIN * 2));
+    GPIOG->MODER |=   0x1 << (HALT_LED_PIN * 2) ;
+
+    /* set (low) speed for pin */
+    GPIOG->OSPEEDR &= ~(0x3 << (HALT_LED_PIN * 2));
+
+    /* set pin as neither pull up nor pull down */
+    GPIOG->PUPDR &= ~(0x3 << (HALT_LED_PIN * 2));
+
+    flag = 0;
+    while(1)
+    {
+        for(toggle = 0; toggle < 2 * (uint32_t) reason; toggle++)
+        {
+            /* toggle PORTG LED pin */
+            GPIOG->BSRR |= 0x1 << (HALT_LED_PIN + (flag ? 16 : 0));
+            flag = !flag;
+            for(delay = 0; delay < HALT_INTRA_PATTERN_DELAY; delay++);
+        }
+        for(delay = 0; delay < HALT_INTER_PATTERN_DELAY; delay++);
+    }
 }
