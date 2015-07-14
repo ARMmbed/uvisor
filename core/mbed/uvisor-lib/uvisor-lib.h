c60c9298ab510d48b3d89ed6551316219baf746d
Alessandro Angelino <alessandro.angelino@arm.com>
2015-07-14 15:44:04 +0100

    uvisor-lib fallback header files for unsupported platforms
    Since some of the features provided by uvisor are needed also in cases where
    uvisor is disabled or not supported, this commit introduces a fallback version of
    the uvisor header files. In particular:
    
    - List of supported platforms is hardcoded in a single easily mantainable file
      (`platforms.h`), that defines if uvisor is supported or not (`UVISOR_PRESENT`)
    - Most of the header files are conditionally included only if `UVISOR_PRESENT`
    - A fallback `unsupported.h` header file provides definitions for functions and
      names independently from the uvisor state:
        - secure gateways are mapped to regular function calls
        - bitband APIs are mapped to regular bitband accesses
        - box configurations are not redirected to uvisor-specific memory sections
          and are just left untouched
    - All code written to work with uvisor will also work on platforms for which
      uvisor is not (yet) supported, with no security features (a warning is
      issued)

--- core/mbed/uvisor-lib/uvisor-lib.h
+++ core/mbed/uvisor-lib/uvisor-lib.h
@@ -14,14 +14,29 @@
 #define __UVISOR_LIB_H__
 
 #include <stdint.h>
+
+/* the symbol UVISOR_PRESENT is defined here based on the supported platforms */
+#include "uvisor-lib/platforms.h"
+
+/* these header files are included independently from the platform */
 #include "uvisor-lib/uvisor_exports.h"
-#include "uvisor-lib/svc_exports.h"
-#include "uvisor-lib/svc_gw_exports.h"
 #include "uvisor-lib/vmpu_exports.h"
+
+/* conditionally included header files */
+#ifdef  UVISOR_PRESENT
+
+#include "uvisor-lib/benchmark.h"
+#include "uvisor-lib/bitband.h"
 #include "uvisor-lib/config.h"
 #include "uvisor-lib/interrupts.h"
 #include "uvisor-lib/secure_gateway.h"
-#include "uvisor-lib/bitband.h"
-#include "uvisor-lib/benchmark.h"
+#include "uvisor-lib/svc_exports.h"
+#include "uvisor-lib/svc_gw_exports.h"
+
+#else /*UVISOR_PRESENT*/
+
+#include "uvisor-lib/unsupported.h"
+
+#endif/*UVISOR_PRESENT*/
 
 #endif/*__UVISOR_LIB_H__*/
