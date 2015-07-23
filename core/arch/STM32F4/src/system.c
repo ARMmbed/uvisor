4ee21b5fdc21420116ea328f8141c2c1c76bfbe9
Alessandro Angelino <alessandro.angelino@arm.com>
2015-07-23 14:47:02 +0100

    Make system.c (stm32f429) more editor firendly
    Aligned columns make visual block moving/editing easier

--- core/arch/STM32F4/src/system.c
+++ core/arch/STM32F4/src/system.c
@@ -25,96 +25,96 @@ void UVISOR_ALIAS(isr_default_sys_handler) SVCall_IRQn_Handler(void);
 void UVISOR_ALIAS(isr_default_sys_handler) DebugMonitor_IRQn_Handler(void);
 void UVISOR_ALIAS(isr_default_sys_handler) PendSV_IRQn_Handler(void);
 void UVISOR_ALIAS(isr_default_sys_handler) SysTick_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) WWDG_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) PVD_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TAMP_STAMP_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) RTC_WKUP_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) FLASH_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) RCC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI0_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI2_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI3_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI4_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream0_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream2_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream3_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream4_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream5_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream6_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) ADC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN1_TX_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN1_RX0_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN1_RX1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN1_SCE_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI9_5_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM1_BRK_TIM9_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM1_UP_TIM10_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM1_TRG_COM_TIM11_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM1_CC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM2_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM3_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM4_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) I2C1_EV_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) I2C1_ER_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) I2C2_EV_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) I2C2_ER_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SPI1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SPI2_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) USART1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) USART2_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) USART3_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) EXTI15_10_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) RTC_Alarm_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) OTG_FS_WKUP_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM8_BRK_TIM12_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM8_UP_TIM13_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM8_TRG_COM_TIM14_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM8_CC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA1_Stream7_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) FMC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SDIO_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM5_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SPI3_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) UART4_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) UART5_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM6_DAC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) TIM7_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream0_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream2_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream3_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream4_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) ETH_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) ETH_WKUP_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN2_TX_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN2_RX0_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN2_RX1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) CAN2_SCE_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) OTG_FS_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream5_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream6_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2_Stream7_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) USART6_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) I2C3_EV_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) I2C3_ER_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) OTG_HS_EP1_OUT_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) OTG_HS_EP1_IN_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) OTG_HS_WKUP_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) OTG_HS_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DCMI_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) HASH_RNG_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) FPU_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) UART7_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) UART8_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SPI4_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SPI5_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SPI6_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) SAI1_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) LTDC_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) LTDC_ER_IRQn_Handler(void);
-void UVISOR_ALIAS(isr_default_handler) DMA2D_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     WWDG_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     PVD_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TAMP_STAMP_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     RTC_WKUP_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     FLASH_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     RCC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI0_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI2_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI3_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI4_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream0_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream2_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream3_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream4_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream5_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream6_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     ADC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN1_TX_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN1_RX0_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN1_RX1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN1_SCE_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI9_5_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM1_BRK_TIM9_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM1_UP_TIM10_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM1_TRG_COM_TIM11_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM1_CC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM2_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM3_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM4_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     I2C1_EV_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     I2C1_ER_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     I2C2_EV_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     I2C2_ER_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SPI1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SPI2_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     USART1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     USART2_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     USART3_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     EXTI15_10_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     RTC_Alarm_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     OTG_FS_WKUP_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM8_BRK_TIM12_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM8_UP_TIM13_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM8_TRG_COM_TIM14_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM8_CC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA1_Stream7_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     FMC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SDIO_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM5_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SPI3_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     UART4_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     UART5_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM6_DAC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     TIM7_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream0_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream2_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream3_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream4_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     ETH_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     ETH_WKUP_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN2_TX_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN2_RX0_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN2_RX1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     CAN2_SCE_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     OTG_FS_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream5_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream6_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2_Stream7_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     USART6_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     I2C3_EV_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     I2C3_ER_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     OTG_HS_EP1_OUT_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     OTG_HS_EP1_IN_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     OTG_HS_WKUP_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     OTG_HS_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DCMI_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     HASH_RNG_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     FPU_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     UART7_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     UART8_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SPI4_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SPI5_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SPI6_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     SAI1_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     LTDC_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     LTDC_ER_IRQn_Handler(void);
+void UVISOR_ALIAS(isr_default_handler)     DMA2D_IRQn_Handler(void);
 
 /* vector table; it will be placed in Flash */
 __attribute__((aligned(256)))
@@ -130,13 +130,13 @@ const TIsrVector g_isr_vector[ISR_VECTORS] =
 	MemoryManagement_IRQn_Handler,       /* -12 */
 	BusFault_IRQn_Handler,               /* -11 */
 	UsageFault_IRQn_Handler,             /* -10 */
-	isr_default_sys_handler,             /*  -9 */
-	isr_default_sys_handler,             /*  -8 */
-	isr_default_sys_handler,             /*  -7 */
-	isr_default_sys_handler,             /*  -6 */
+	isr_default_sys_handler,             /* - 9 */
+	isr_default_sys_handler,             /* - 8 */
+	isr_default_sys_handler,             /* - 7 */
+	isr_default_sys_handler,             /* - 6 */
 	SVCall_IRQn_Handler,                 /* - 5 */
 	DebugMonitor_IRQn_Handler,           /* - 4 */
-	isr_default_sys_handler,             /*  -3 */
+	isr_default_sys_handler,             /* - 3 */
 	PendSV_IRQn_Handler,                 /* - 2 */
 	SysTick_IRQn_Handler,                /* - 1 */
 
