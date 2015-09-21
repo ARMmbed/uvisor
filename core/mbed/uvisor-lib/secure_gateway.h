da9eeb4420cc7bbbf0338b3c650862ea072df476
Alessandro Angelino <alessandro.angelino@arm.com>
2015-09-21 11:38:41 +0100

    Moved macros for asm to uvisor_exports
    These macros will be used system-wide every time as utilities (string-ification,
    macro overloading, etc.)

--- core/mbed/uvisor-lib/secure_gateway.h
+++ core/mbed/uvisor-lib/secure_gateway.h
@@ -17,51 +17,6 @@
 #ifndef __SECURE_GATEWAY_H__
 #define __SECURE_GATEWAY_H__
 
-/* this macro selects an overloaded macro (variable number of arguments) */
-#define SELECT_MACRO(_0, _1, _2, _3, _4, NAME, ...) NAME
-
-/* used to count macro arguments */
-#define NARGS(...)                                                             \
-     SELECT_MACRO(_0, ##__VA_ARGS__, 4, 3, 2, 1, 0)
-
-/* used to declare register values to hold the variable number of arguments */
-#define SELECT_ARGS(...)                                                       \
-     SELECT_MACRO(_0, ##__VA_ARGS__, SELECT_ARGS4,                             \
-                                     SELECT_ARGS3,                             \
-                                     SELECT_ARGS2,                             \
-                                     SELECT_ARGS1,                             \
-                                     SELECT_ARGS0)(__VA_ARGS__)                \
-
-#define SELECT_ARGS0()
-#define SELECT_ARGS1(a0)                                                       \
-        register uint32_t r0 asm("r0") = (uint32_t) a0;
-#define SELECT_ARGS2(a0, a1)                                                   \
-        register uint32_t r0 asm("r0") = (uint32_t) a0;                        \
-        register uint32_t r1 asm("r1") = (uint32_t) a1;
-#define SELECT_ARGS3(a0, a1, a2)                                               \
-        register uint32_t r0 asm("r0") = (uint32_t) a0;                        \
-        register uint32_t r1 asm("r1") = (uint32_t) a1;                        \
-        register uint32_t r2 asm("r2") = (uint32_t) a2;
-#define SELECT_ARGS4(a0, a1, a2, a3)                                           \
-        register uint32_t r0 asm("r0") = (uint32_t) a0;                        \
-        register uint32_t r1 asm("r1") = (uint32_t) a1;                        \
-        register uint32_t r2 asm("r2") = (uint32_t) a2;                        \
-        register uint32_t r3 asm("r3") = (uint32_t) a3;
-
-/* used to declare registers in the asm volatile to avoid compiler opt */
-#define SELECT_REGS(...) \
-     SELECT_MACRO(_0, ##__VA_ARGS__, SELECT_REGS4,                             \
-                                     SELECT_REGS3,                             \
-                                     SELECT_REGS2,                             \
-                                     SELECT_REGS1,                             \
-                                     SELECT_REGS0)(__VA_ARGS__)                \
-
-#define SELECT_REGS0()
-#define SELECT_REGS1(a0)             , "r" (r0)
-#define SELECT_REGS2(a0, a1)         , "r" (r0), "r" (r1)
-#define SELECT_REGS3(a0, a1, a2)     , "r" (r0), "r" (r1), "r" (r2)
-#define SELECT_REGS4(a0, a1, a2, a3) , "r" (r0), "r" (r1), "r" (r2), "r" (r3)
-
 /* the actual secure gateway */
 #define secure_gateway(dst_box, dst_fn, ...)                                   \
     ({                                                                         \
