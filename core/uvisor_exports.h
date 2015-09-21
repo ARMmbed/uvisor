87941a02e9af8275eb94639216d934aacf5bfc52
Alessandro Angelino <alessandro.angelino@arm.com>
2015-09-21 11:33:36 +0100

    Added attribute to force inlining
    Also moved max box number configuration to the top

--- core/uvisor_exports.h
+++ core/uvisor_exports.h
@@ -17,25 +17,28 @@
 #ifndef __UVISOR_EXPORTS_H__
 #define __UVISOR_EXPORTS_H__
 
+/* maximum number of boxes allowed: 1 is the minimum (unprivileged box) */
+#define UVISOR_MAX_BOXES 5U
+
+/* extern keyword */
 #ifdef  __cplusplus
 #define UVISOR_EXTERN extern "C"
 #else
 #define UVISOR_EXTERN extern
 #endif/*__CPP__*/
 
-#define UVISOR_NOINLINE  __attribute__((noinline))
-#define UVISOR_PACKED    __attribute__((packed))
-#define UVISOR_WEAK      __attribute__((weak))
-#define UVISOR_ALIAS(f)  __attribute__((weak, alias (#f)))
-#define UVISOR_LINKTO(f) __attribute__((alias (#f)))
-#define UVISOR_NORETURN  __attribute__((noreturn))
-#define UVISOR_NAKED     __attribute__((naked))
-#define UVISOR_RAMFUNC   __attribute__ ((section (".ramfunc"), noinline))
+/* compiler attributes */
+#define UVISOR_FORCEINLINE __attribute__((always_inline))
+#define UVISOR_NOINLINE    __attribute__((noinline))
+#define UVISOR_PACKED      __attribute__((packed))
+#define UVISOR_WEAK        __attribute__((weak))
+#define UVISOR_ALIAS(f)    __attribute__((weak, alias (#f)))
+#define UVISOR_LINKTO(f)   __attribute__((alias (#f)))
+#define UVISOR_NORETURN    __attribute__((noreturn))
+#define UVISOR_NAKED       __attribute__((naked))
+#define UVISOR_RAMFUNC     __attribute__ ((section (".ramfunc"), noinline))
 
 /* array count macro */
 #define UVISOR_ARRAY_COUNT(x) (sizeof(x)/sizeof(x[0]))
-
-/* maximum number of boxes allowed: 1 is the minimum (unprivileged box) */
-#define UVISOR_MAX_BOXES 5U
 
 #endif/*__UVISOR_EXPORTS_H__*/
