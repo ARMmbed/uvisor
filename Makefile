ARMmbed/uvisor

commit 327a925d848c68394185f67c0b94121fb4b3aef7

Fix bug in makefile for debug/release builds
If the objects file are not removed between single builds, they are
reused across builds for the same configuration (debug/release) and
across builds for different configurations in the same platform.

Removing them makes sure that a new build is always triggered in the 2
cases above.

Fixes: #157 ("Release mode binaries aren't actually built in release mode")

Makefile
@@ -186,7 +186,7 @@ BINARY_CONFIG:=\
     -DUVISOR_MAGIC=$(UVISOR_MAGIC) \
     -DUVISOR_BIN=\"$(CONFIGURATION_PREFIX).bin\"
 
-.PHONY: all fresh __platform/% __configurations __binaries CONFIGURATION_% ctags source.c.tags
+.PHONY: all fresh __platform/% __configurations __binaries CONFIGURATION_% __rm_objs clean source.c.tags
 
 # Build both the release and debug versions for all platforms for all
 # configurations.
@@ -213,17 +213,18 @@ __configurations: $(CONFIGURATIONS)
 
 # This target is used to iterate over all configurations for the current
 # platform.
+# Note: We need to remove the object files from a previous build or otherwise
+# the old configurations will be used.
 CONFIGURATION_%:
 ifndef PLATFORM
 	$(error "Missing platform. Use PLATFORM=<your_platform> make CONFIGURATION_<your_configuration>")
 else
-	make CONFIGURATION=$@ __binaries yotta
+	make CONFIGURATION=$@ __rm_objs __binaries yotta
 endif
 
 # This middleware target is needed because the parent make does not know the
 # name to give to the binary release yet (it is configuration-specific).
 __binaries: $(BINARY_RELEASE)
-	@echo $(BINARY_RELEASE)
 
 # Generate the pre-linked uVisor binary for the given platform and configuration.
 $(BINARY_RELEASE): $(CONFIGURATION_PREFIX).bin
@@ -262,6 +263,9 @@ ctags: source.c.tags
 
 source.c.tags: $(SOURCES)
 	CFLAGS="$(CFLAGS_PRE)" geany -g $@ $^
+
+__rm_objs:
+	find $(ROOT_DIR) -iname '*.o' -not -path "$(MBED_DIR)/*" -delete
 
 clean:
 	rm -f source.c.tags

commit a897d616377e80398ab89f9fae9ea8028ae4acc3

Fix bug in Makefile
The removal of object files between builds must exclude the release
folder as well.

Fixes: 327a925 ("Fix bug in makefile for debug/release builds")

Makefile
@@ -265,7 +265,7 @@ source.c.tags: $(SOURCES)
 	CFLAGS="$(CFLAGS_PRE)" geany -g $@ $^
 
 __rm_objs:
-	find $(ROOT_DIR) -iname '*.o' -not -path "$(MBED_DIR)/*" -delete
+	find $(ROOT_DIR) -iname '*.o' -not -path "$(RELEASE_DIR)/*" -not -path "$(MBED_DIR)/*" -delete
 
 clean:
 	rm -f source.c.tags