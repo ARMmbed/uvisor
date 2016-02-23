ARMmbed/uvisor

commit e54ca29f3168e7c4adba6b628ce4f74b03839711

ensure that minimum stack size is reserved during linking process

core/system/src/mpu/vmpu_armv7m.c
@@ -67,7 +67,7 @@ typedef struct {
     uint32_t count;
 } TMpuBox;
 
-uint32_t g_mpu_region_count;
+uint32_t g_mpu_region_count, g_box_mem_pos;
 TMpuRegion g_mpu_list[MPU_REGION_COUNT];
 TMpuBox g_mpu_box[UVISOR_MAX_BOXES];
 
@@ -263,84 +263,82 @@ void vmpu_init_static_regions(void)
     MPU->CTRL = MPU_CTRL_ENABLE_Msk|MPU_CTRL_PRIVDEFENA_Msk;
 }
 
-void vmpu_initialize_stacks(
-    const TBoxMemorySize* box,
-    void *stack_start, void *stack_end)
+void vmpu_acl_stack(uint8_t box_id, uint32_t context_size, uint32_t stack_size)
 {
-    int box_id, bits, slots_ctx, slots_stack;
-    uint32_t size, block_size, stack_size, stack_addr;
+    int bits, slots_ctx, slots_stack;
+    uint32_t size, block_size;
 
-    DPRINTF("\n\rbox stack segment start=0x%08X end=0x%08X (length=%i)\n\r",
-        stack_start, stack_end,
-        ((uint32_t)stack_end)-((uint32_t)stack_start));
-
-    /* assign main box stack pointer to existing
-     * unprivileged stack pointer */
-    g_svc_cx_curr_sp[0] = (uint32_t*)__get_PSP();
-    g_svc_cx_context_ptr[0] = NULL;
-    box++;
-
-    /* process remaining boxes */
-    stack_addr = (uint32_t)stack_start;
-    for(box_id=1; box_id<g_vmpu_box_count; box_id++)
+    /* handle main box */
+    if(box_id)
+        stack_size = UVISOR_MIN_STACK(stack_size);
+    else
     {
-        /* ensure that box stack is at least UVISOR_BOX_STACK_SIZE */
-        stack_size = (box->stack<=UVISOR_BOX_STACK_SIZE) ?
-            UVISOR_BOX_STACK_SIZE : box->stack;
+        DPRINTF("ctx=%i stack=%i\n\r", context_size, stack_size);
+        /* non-important sanity checks */
+        assert(context_size == 0);
+        assert(stack_size == 0);
 
-        /* ensure that 2/8th are available for protecting stack from
-         * context - include rounding error margin */
-        bits = vmpu_region_bits(((stack_size + box->context)*8)/6);
-
-        /* ensure MPU region size of at least 256 bytes for
-         * subregion support */
-        if(bits<8)
-            bits = 8;
-        size = 1UL << bits;
-        block_size = size/8;
-
-        DPRINTF("\tbox[%i] stack=%i context=%i rounded=%i " , box_id,
-            box->stack, box->context, size);
-
-        /* check for correct context address alignment:
-         * alignment needs to be a muiltiple of the size */
-        if((stack_addr & (size-1))!=0)
-            stack_addr = (stack_addr & ~(size-1)) + size;
-
-        /* check if we have enough memory left */
-        if((stack_addr + size)>(uint32_t)stack_end)
-            HALT_ERROR(SANITY_CHECK_FAILED,
-                "memory overflow - increase uvisor memory "
-                "allocation\n\r");
-
-        /* round context sizes, leave one free slot */
-        slots_ctx = (box->context + block_size - 1)/block_size;
-        slots_stack = 8-slots_ctx-1;
-
-        /* final sanity checks */
-        if((slots_ctx * block_size)<box->context)
-            HALT_ERROR(SANITY_CHECK_FAILED, "slots_ctx underrun\n\r");
-        if((slots_stack * block_size)<stack_size)
-            HALT_ERROR(SANITY_CHECK_FAILED, "slots_stack underrun\n\r");
-
-        /* allocate context pointer */
-        g_svc_cx_context_ptr[box_id] = (uint32_t*)stack_addr;
-        /* ensure stack band on top for stack underflow dectection */
-        g_svc_cx_curr_sp[box_id] =
-            (uint32_t*)(stack_addr + size - UVISOR_STACK_BAND_SIZE);
-        /* debug stack & context allocation */
-        DPRINTF("ctx=0x%08X sp=0x%08X\n\r",
-            g_svc_cx_context_ptr[box_id],
-            g_svc_cx_curr_sp[box_id]);
-
-        box++;
-        stack_addr+=size;
+        /* assign main box stack pointer to existing
+         * unprivileged stack pointer */
+        g_svc_cx_curr_sp[0] = (uint32_t*)__get_PSP();
+        g_svc_cx_context_ptr[0] = NULL;
+        return;
     }
 
-    DPRINTF("\n\r");
+    /* ensure that box stack is at least UVISOR_MIN_STACK_SIZE */
+    if(stack_size<UVISOR_MIN_STACK_SIZE)
+        stack_size = UVISOR_MIN_STACK_SIZE;
+
+    /* ensure that 2/8th are available for protecting stack from
+     * context - include rounding error margin */
+    bits = vmpu_region_bits(((stack_size + context_size)*8)/6);
+
+    /* ensure MPU region size of at least 256 bytes for
+     * subregion support */
+    if(bits<8)
+        bits = 8;
+    size = 1UL << bits;
+    block_size = size/8;
+
+    DPRINTF("\tbox[%i] stack=%i context=%i rounded=%i " , box_id,
+        stack_size, context_size, size);
+
+    /* check for correct context address alignment:
+     * alignment needs to be a muiltiple of the size */
+    if( (g_box_mem_pos & (size-1))!=0 )
+        g_box_mem_pos = (g_box_mem_pos & ~(size-1)) + size;
+
+    /* check if we have enough memory left */
+    if((g_box_mem_pos + size)>((uint32_t)__uvisor_config.bss_start))
+        HALT_ERROR(SANITY_CHECK_FAILED,
+            "memory overflow - increase uvisor memory "
+            "allocation\n\r");
+
+    /* round context sizes, leave one free slot */
+    slots_ctx = (context_size + block_size - 1)/block_size;
+    slots_stack = 8-slots_ctx-1;
+
+    /* final sanity checks */
+    if( (slots_ctx * block_size) < context_size )
+        HALT_ERROR(SANITY_CHECK_FAILED, "slots_ctx underrun\n\r");
+    if( (slots_stack * block_size) < stack_size )
+        HALT_ERROR(SANITY_CHECK_FAILED, "slots_stack underrun\n\r");
+
+    /* allocate context pointer */
+    g_svc_cx_context_ptr[box_id] = (uint32_t*)g_box_mem_pos;
+    /* ensure stack band on top for stack underflow dectection */
+    g_svc_cx_curr_sp[box_id] =
+        (uint32_t*)(g_box_mem_pos + size - UVISOR_STACK_BAND_SIZE);
+    /* debug stack & context allocation */
+    DPRINTF("ctx=0x%08X sp=0x%08X\n\r",
+        g_svc_cx_context_ptr[box_id],
+        g_svc_cx_curr_sp[box_id]);
+
+    /* move on to the next memory block */
+    g_box_mem_pos += size;
 }
 
-void vmpu_init_protection(void)
+void vmpu_arch_init(void)
 {
     /* reset MPU */
     MPU->CTRL = 0;
@@ -357,6 +355,12 @@ void vmpu_init_protection(void)
     /* sanity checks */
     assert(ARMv7M_MPU_REGIONS == ((MPU->TYPE >> MPU_TYPE_DREGION_Pos) & 0xFF));
     assert(ARMv7M_MPU_ALIGNMENT_BITS == vmpu_bits(g_vmpu_aligment_mask));
+
+    /* init protected box memory enumation pointer */
+    DPRINTF("\n\rbox stack segment start=0x%08X end=0x%08X (length=%i)\n\r",
+        __uvisor_config.reserved_end, __uvisor_config.bss_start,
+        ((uint32_t)__uvisor_config.bss_start)-((uint32_t)__uvisor_config.reserved_end));
+    g_box_mem_pos = (uint32_t)__uvisor_config.reserved_end;
 
     /* enable mem, bus and usage faults */
     SCB->SHCSR |= 0x70000;

core/mbed/uvisor-lib/box_config.h
@@ -58,12 +58,12 @@ UVISOR_EXTERN const uint32_t __uvisor_mode;
 #define __UVISOR_BOX_CONFIG(box_name, acl_list, stack_size, context_size) \
     \
     uint8_t __attribute__((section(".uvisor.bss.stack"), aligned(32))) \
-        box_name ## _reserved[UVISOR_STACK_SIZE_ROUND((((stack_size) + (context_size))*8)/6)]; \
+        box_name ## _reserved[UVISOR_STACK_SIZE_ROUND(((UVISOR_MIN_STACK(stack_size) + (context_size))*8)/6)]; \
     \
     static UVISOR_SECURE_CONST UvisorBoxConfig box_name ## _cfg = { \
         UVISOR_BOX_MAGIC, \
         UVISOR_BOX_VERSION, \
-        stack_size, \
+        UVISOR_MIN_STACK(stack_size), \
         context_size, \
         acl_list, \
         UVISOR_ARRAY_COUNT(acl_list) \

core/system/inc/mpu/vmpu.h
@@ -15,12 +15,6 @@
 
 #include "vmpu_exports.h"
 
-typedef struct
-{
-    uint32_t stack;
-    uint32_t context;
-} TBoxMemorySize;
-
 #define VMPU_FLASH_ADDR_MASK  (~(((uint32_t)(FLASH_LENGTH)) - 1))
 #define VMPU_FLASH_ADDR(addr) ((VMPU_FLASH_ADDR_MASK & (uint32_t)(addr)) == \
                                (FLASH_ORIGIN & VMPU_FLASH_ADDR_MASK))
@@ -43,10 +37,9 @@ extern int  vmpu_switch(uint8_t src_box, uint8_t dst_box);
 
 extern void vmpu_load_box(uint8_t box_id);
 
-extern void vmpu_initialize_stacks(
-    const TBoxMemorySize* box, void *stack_start, void *stack_end);
+extern void vmpu_acl_stack(uint8_t box_id, uint32_t context_size, uint32_t stack_size);
 
-extern void vmpu_init_protection(void);
+extern void vmpu_arch_init(void);
 extern int  vmpu_init_pre(void);
 extern void vmpu_init_post(void);
 

core/system/inc/mpu/vmpu_exports.h
@@ -75,7 +75,8 @@
 #define UVISOR_STACK_BAND_SIZE      128
 #define UVISOR_MEM_SIZE_ROUND(x)    UVISOR_REGION_ROUND_UP(x)
 
-#define UVISOR_MIN_STACK_SIZE       ((UVISOR_STACK_BAND_SIZE)*4)
+#define UVISOR_MIN_STACK_SIZE       1024
+#define UVISOR_MIN_STACK(x)         (((x)<UVISOR_MIN_STACK_SIZE)?UVISOR_MIN_STACK_SIZE:(x))
 
 #ifdef  ARCH_MK64F
 #define UVISOR_REGION_ROUND_DOWN(x) ((x) & ~0x1FUL)
@@ -89,7 +90,7 @@
 #endif/*ARCH_MK64F*/
 
 #ifndef UVISOR_BOX_STACK_SIZE
-#define UVISOR_BOX_STACK_SIZE 1024
+#define UVISOR_BOX_STACK_SIZE UVISOR_MIN_STACK_SIZE
 #endif/*UVISOR_BOX_STACK*/
 
 typedef uint32_t UvisorBoxAcl;

core/system/src/mpu/vmpu.c
@@ -114,10 +114,8 @@ static int vmpu_sanity_checks(void)
 static void vmpu_load_boxes(void)
 {
     int i, count;
-
     const UvisorBoxAclItem *region;
     const UvisorBoxConfig **box_cfgtbl;
-    TBoxMemorySize box_size[UVISOR_MAX_BOXES], *bm;
     uint8_t box_id;
 
     /* enumerate and initialize boxes */
@@ -161,23 +159,13 @@ static void vmpu_load_boxes(void)
         /* load box ACLs in table */
         DPRINTF("box[%i] ACL list:\n", box_id);
 
-        /* update stack pointers */
-        bm = &box_size[box_id];
-        if(!box_id)
-        {
-            bm->stack   =  0;
-            bm->context =  0;
-        }
-        else
-        {
-            /* update box memories list */
-            if((*box_cfgtbl)->stack_size <= UVISOR_MIN_STACK_SIZE)
-                bm->stack = UVISOR_MIN_STACK_SIZE;
-            else
-                bm->stack   =  (*box_cfgtbl)->stack_size;
-
-            bm->context =  (*box_cfgtbl)->context_size;
-        }
+        /* add ACL's for all box stacks, the actual start addesses and
+         * sizes are resolved later in vmpu_initialize_stacks */
+        vmpu_acl_stack(
+            box_id,
+            (*box_cfgtbl)->context_size,
+            (*box_cfgtbl)->stack_size
+        );
 
         region = (*box_cfgtbl)->acl_list;
         if(region)
@@ -211,73 +199,10 @@ static void vmpu_load_boxes(void)
         }
     }
 
-    /* initialize box stacks within stack segment */
-    vmpu_initialize_stacks(
-        box_size,
-        __uvisor_config.reserved_end,
-        __uvisor_config.bss_start
-        );
-
     /* load box 0 */
     vmpu_load_box(0);
 
     DPRINTF("vmpu_load_boxes [DONE]\n");
-}
-
-void UVISOR_WEAK vmpu_load_box(uint8_t box_id)
-{
-    HALT_ERROR(NOT_IMPLEMENTED,
-               "vmpu_load_box needs hw-specific implementation\n\r");
-}
-
-int UVISOR_WEAK vmpu_acl_dev(UvisorBoxAcl acl, uint16_t device_id)
-{
-    HALT_ERROR(NOT_IMPLEMENTED, "vmpu_acl_dev not implemented yet\n\r");
-    return 1;
-}
-
-int UVISOR_WEAK vmpu_acl_mem(UvisorBoxAcl acl, uint32_t addr, uint32_t size)
-{
-    HALT_ERROR(NOT_IMPLEMENTED, "vmpu_acl_mem not implemented yet\n\r");
-    return 1;
-}
-
-int UVISOR_WEAK vmpu_acl_reg(UvisorBoxAcl acl, uint32_t addr, uint32_t rmask,
-                             uint32_t wmask)
-{
-    HALT_ERROR(NOT_IMPLEMENTED, "vmpu_acl_reg not implemented yet\n\r");
-    return 1;
-}
-
-int UVISOR_WEAK vmpu_acl_bit(UvisorBoxAcl acl, uint32_t addr)
-{
-    HALT_ERROR(NOT_IMPLEMENTED, "vmpu_acl_bit not implemented yet\n\r");
-    return 1;
-}
-
-void UVISOR_WEAK vmpu_acl_irq(uint8_t box_id, void* function, uint32_t isr_id)
-{
-    HALT_ERROR(NOT_IMPLEMENTED,
-               "vmpu_acl_irq needs hw-specific implementation\n\r");
-}
-
-void UVISOR_WEAK vmpu_acl_add(uint8_t box_id, void* addr, uint32_t size, UvisorBoxAcl acl)
-{
-    HALT_ERROR(NOT_IMPLEMENTED,
-               "vmpu_acl_add needs hw-specific implementation\n\r");
-}
-
-int UVISOR_WEAK vmpu_switch(uint8_t src_box, uint8_t dst_box)
-{
-    HALT_ERROR(NOT_IMPLEMENTED,
-               "vmpu_switch needs hw-specific implementation\n\r");
-    return 1;
-}
-
-void UVISOR_WEAK vmpu_init_protection(void)
-{
-    HALT_ERROR(NOT_IMPLEMENTED,
-               "vmpu_init_protection needs hw-specific implementation\n\r");
 }
 
 int vmpu_init_pre(void)
@@ -321,7 +246,7 @@ void vmpu_init_post(void)
     __uvisor_box_context = (void *) __uvisor_config.uvisor_box_context;
 
     /* init memory protection */
-    vmpu_init_protection();
+    vmpu_arch_init();
 
     /* load boxes */
     vmpu_load_boxes();

commit 5fc339b0e9300344c63a9a79577955756c8ce650

Add missing secure context reset for ARMv7-M vMPU
The private box contexts were not zeroed at initialization time.

Fixes: e54ca29 ("added subregion stack bands for protecting box context from box stack")

core/system/src/mpu/vmpu_armv7m.c
@@ -575,6 +575,11 @@ void vmpu_acl_stack(uint8_t box_id, uint32_t context_size, uint32_t stack_size)
     g_svc_cx_curr_sp[box_id] =
         (uint32_t*)(g_box_mem_pos + size - UVISOR_STACK_BAND_SIZE);
 
+    /* Reset uninitialized secured box context. */
+    if (slots_ctx) {
+        memset((void *) g_box_mem_pos, 0, context_size);
+    }
+
     /* create stack protection region */
     vmpu_acl_add(
         box_id, (void*)g_box_mem_pos, size,