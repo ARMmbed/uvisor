ARMmbed/uvisor

commit 9fdcffd6fd6718b0e8364e162e32c342ffb86595

Add core_generic.h to select correct CMSIS headers
Without hw-specific header files, we need to configure the CMSIS headers
manually. This means that we must:
- Specify the core version, so that the correct header file is used. Note: This
  results in the core version being one additional axis for the generation of
  the uVisor binary releases.
- Set the __FPU_PRESENT macro by default to 0. Note: When FPU will be supported
  we'll need to detect it runtime.
- Redefine the __NVIC_PRIO_BITS to a global variable, set by uVisor at startup.
- Define MPU_PRESENT to 1 because we trust we only ported a platform to uVisor
  if it has an MPU.

core/cmsis/inc/core_generic.h
@@ -1,0 +1,79 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __CORE_GENERIC_H__
+#define __CORE_GENERIC_H__
+
+#include <stdint.h>
+
+/* CMSIS core configuration
+ * Note: since uVisor is hw-agnostic, we must be conservative. */
+
+/* MPU configuration
+ * Always 1, since we require an MPU, unless it's a Kinetis MPU, in which case
+ * we use theirs. */
+#if defined(ARCH_MPU_ARMv7M)
+#define __MPU_PRESENT 1
+#elif defined(ARCH_MPU_KINETIS)
+#else
+#define __MPU_PRESENT 0
+#error "Unknown MPU architecture. Check your Makefile."
+#endif /* defined(ARCH_MPU_ARMv7M) || defined(ARCH_MPU_KINETIS) */
+
+/* We don't need the SysTick configurtion. */
+#define __Vendor_SysTickConfig 0
+
+/* At compile time uVisor does not need to know if the device has an FPU. It
+ * will be detected at runtime. */
+#define __FPU_PRESENT 0
+
+/* Redefine the macro to a custom variable that is written at startup time by
+ * uVisor.
+ * This allows us to remove the __NVIC_PRIO_BITS axis from the release binaries
+ * matrix */
+extern uint8_t g_nvic_prio_bits;
+#define __NVIC_PRIO_BITS g_nvic_prio_bits
+
+/* Core interrupts */
+typedef enum IRQn {
+  NonMaskableInt_IRQn   = -14,
+  HardFault_IRQn        = -13,
+  MemoryManagement_IRQn = -12,
+  BusFault_IRQn         = -11,
+  UsageFault_IRQn       = -10,
+  SVCall_IRQn           = -5,
+  DebugMonitor_IRQn     = -4,
+  PendSV_IRQn           = -2,
+  SysTick_IRQn          = -1,
+} IRQn_Type;
+
+/* Include the CMSIS core header files.
+ * The Cortex-M specification must be defined by the release configuration. */
+/* FIXME The Cortex-M3 and -M4 header files also differ for the fact that the
+ *       SCnSCB->ACTRL was introduced with REV 0x200. All bus faults on a
+ *       Cortex-M3 device produced before that revision will be imprecise. */
+#if defined(CORE_CORTEX_M3) || defined(CORE_CORTEX_M4)
+#include "core_cm4.h"
+#else /* defined(CORE_CORTEX_M3) || defined(CORE_CORTEX_M4) */
+#error "Unsupported ARM core. Make sure CORE_* is defined in your workspace."
+#endif /* unsupported ARM core */
+
+/* Optional MPU headers for the NXP Kinetis MPU */
+#ifdef ARCH_MPU_KINETIS
+#include "mpu_kinetis.h"
+#endif /* ARCH_MPU_KINETIS */
+
+#endif /* __CORE_GENERIC_H__ */

core/system/src/main.c
@@ -38,15 +38,15 @@ UVISOR_NOINLINE void uvisor_init_pre(void)
         VMPU_REGION_SIZE(&__data_start__, &__data_end__)
     );
 
+    /* Initialize the unprivileged NVIC module. */
+    unvic_init();
+
     /* initialize debugging features */
     DEBUG_INIT();
 }
 
 UVISOR_NOINLINE void uvisor_init_post(void)
 {
-        /* vector table initialization */
-        unvic_init();
-
         /* init MPU */
         vmpu_init_post();
 

core/system/src/unvic.c
@@ -22,6 +22,7 @@
 
 /* unprivileged vector table */
 TIsrUVector g_unvic_vector[NVIC_VECTORS];
+uint8_t g_nvic_prio_bits;
 
 /* vmpu_acl_irq to unvic_acl_add */
 void vmpu_acl_irq(uint8_t box_id, void *function, uint32_t irqn)
@@ -442,6 +443,15 @@ void __unvic_svc_cx_out(uint32_t *svc_sp, uint32_t *msp)
 
 void unvic_init(void)
 {
+    /* Detect the number of implemented priority bits. */
+    __disable_irq();
+    *((volatile uint8_t *) &(NVIC->IP[0])) = 0xFFU;
+    g_nvic_prio_bits = (uint8_t) __builtin_popcount(*((volatile uint8_t *) &(NVIC->IP[0])));
+    __enable_irq();
+
+    /* Verify that the priority bits read at runtime are realistic. */
+    assert(g_nvic_prio_bits > 0 && g_nvic_prio_bits <= 8);
+
     /* check that minimum priority is still in the range of possible priority
      * levels */
     assert(__UVISOR_NVIC_MIN_PRIORITY < UVISOR_VIRQ_MAX_PRIORITY);

core/uvisor.h
@@ -17,13 +17,21 @@
 #ifndef __UVISOR_H__
 #define __UVISOR_H__
 
-/* definitions that are made visible externally */
-#include "uvisor_exports.h"
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+
+/* The platform configuration file must be included first. */
+#include "config.h"
+
+/* uVisor configurations */
 #include "uvisor-config.h"
+
+/* CMSIS header files */
+#include "core_generic.h"
+
+/* definitions that are made visible externally */
+#include "uvisor_exports.h"
 
 #ifndef TRUE
 #define TRUE 1
@@ -40,9 +48,6 @@
 
 /* unprivileged box as called by privileged code */
 typedef void (*UnprivilegedBoxEntry)(void);
-
-/* per-project-configuration */
-#include <config.h>
 
 #ifdef  NDEBUG
 #define DPRINTF(...) {}

commit d3cf4afe7703306798503612f09c3abe4c28f0af

Fix misplaced macro definition in core_generic.h
Fixes: 9fdcffd ("Add core_generic.h to select correct CMSIS headers")

core/cmsis/inc/core_generic.h
@@ -28,8 +28,8 @@
 #if defined(ARCH_MPU_ARMv7M)
 #define __MPU_PRESENT 1
 #elif defined(ARCH_MPU_KINETIS)
-#else
 #define __MPU_PRESENT 0
+#else
 #error "Unknown MPU architecture. Check your Makefile."
 #endif /* defined(ARCH_MPU_ARMv7M) || defined(ARCH_MPU_KINETIS) */
 