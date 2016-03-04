ARMmbed/uvisor

commit ba189664d5940f44f78d41d2d49865e2b4cb31e6

Move fault status register clearing to end of handler
This allows the fault-related registers to be still valid for the rest of the handler execution, where
they could be used for debugging purposes. The flafgs are always cleared right before returning from
the exception handler

core/system/src/mpu/vmpu_armv7m.c
@@ -185,13 +185,13 @@ void vmpu_sys_mux_handler(uint32_t lr)
                 sp = __get_PSP();
                 pc = vmpu_unpriv_uint32_read(sp + (6 * 4));
 
-                /* backup fault address and status, then clear the MMARVALID flag */
+                /* backup fault address and status */
                 fault_addr = SCB->MMFAR;
                 fault_status = VMPU_SCB_MMFSR;
-                VMPU_SCB_MMFSR = fault_status;
 
                 /* check if the fault is an MPU fault */
                 if (vmpu_fault_recovery_mpu(pc, sp, fault_addr, fault_status)) {
+                    VMPU_SCB_MMFSR = fault_status;
                     return;
                 }
 
@@ -221,14 +221,15 @@ void vmpu_sys_mux_handler(uint32_t lr)
                 sp = __get_PSP();
                 pc = vmpu_unpriv_uint32_read(sp + (6 * 4));
 
-                /* backup fault address and status, then clear the BFARVALID flag */
+                /* backup fault address and status */
                 fault_addr = SCB->BFAR;
                 fault_status = VMPU_SCB_BFSR;
-                VMPU_SCB_BFSR = fault_status;
 
                 /* check if the fault is the special register corner case */
-                if(!vmpu_fault_recovery_bus(pc, sp, fault_addr, fault_status))
+                if (!vmpu_fault_recovery_bus(pc, sp, fault_addr, fault_status)) {
+                    VMPU_SCB_BFSR = fault_status;
                     return;
+                }
 
                 /* if recovery was not successful, throw an error and halt */
                 DEBUG_FAULT(FAULT_BUS, lr);

core/system/src/mpu/vmpu_freescale_k64.c
@@ -71,18 +71,21 @@ void vmpu_sys_mux_handler(uint32_t lr)
                 sp = __get_PSP();
                 pc = vmpu_unpriv_uint32_read(sp + (6 * 4));
 
-                /* backup fault address and status, then clear the BFARVALID flag */
+                /* backup fault address and status */
                 fault_addr = SCB->BFAR;
                 fault_status = VMPU_SCB_BFSR;
-                VMPU_SCB_BFSR = fault_status;
 
                 /* check if the fault is an MPU fault */
-                if(MPU->CESR >> 27 && !vmpu_fault_recovery_mpu(pc, sp))
+                if (MPU->CESR >> 27 && !vmpu_fault_recovery_mpu(pc, sp)) {
+                    VMPU_SCB_BFSR = fault_status;
                     return;
+                }
 
                 /* check if the fault is the special register corner case */
-                if(!vmpu_fault_recovery_bus(pc, sp, fault_addr, fault_status))
+                if (!vmpu_fault_recovery_bus(pc, sp, fault_addr, fault_status)) {
+                    VMPU_SCB_BFSR = fault_status;
                     return;
+                }
 
                 /* if recovery was not successful, throw an error and halt */
                 DEBUG_FAULT(FAULT_BUS, lr);

commit 6e354f1640369b521d015f3a4fdaa5a35241a974

Clear fault status before halting
Now that halt functions allow re-directing execution to an unprivileged
handler, we need to make sure that the bus fault status is cleared
before halting, otherwise two consecutive faults would have a spurious
state.

Fixes: ba18966 Move fault status register clearing to end of handler

core/system/src/mpu/vmpu_armv7m.c
@@ -230,6 +230,7 @@ void vmpu_sys_mux_handler(uint32_t lr, uint32_t msp)
 
                 /* if recovery was not successful, throw an error and halt */
                 DEBUG_FAULT(FAULT_MEMMANAGE, lr, psp);
+                VMPU_SCB_MMFSR = fault_status;
                 HALT_ERROR(PERMISSION_DENIED, "Access to restricted resource denied");
             }
             else

core/system/src/mpu/vmpu_freescale_k64.c
@@ -90,6 +90,7 @@ void vmpu_sys_mux_handler(uint32_t lr, uint32_t msp)
 
                 /* if recovery was not successful, throw an error and halt */
                 DEBUG_FAULT(FAULT_BUS, lr, psp);
+                VMPU_SCB_BFSR = fault_status;
                 HALT_ERROR(PERMISSION_DENIED, "Access to restricted resource denied");
             }
             else