61d0a1c27d8df9666a292e833f09b710e538eda5
Alessandro Angelino <alessandro.angelino@arm.com>
2016-05-05 16:45:07 +0100

    Turn ADDRESS_WRITE into a static inline function
    This prevents compiler's problems observed with IAR. In that case, if
    ADDRESS_WRITE was called wrapped in parenthesis the ({ ... }) GNU
    extension was mistakenly triggered which required a return value.
    
    This implementation is cleaner and makes sure that the macro results in
    a proper function call.

--- api/inc/secure_access.h
+++ api/inc/secure_access.h
@@ -19,42 +19,8 @@
 
 #include "api/inc/uvisor_exports.h"
 #include "api/inc/vmpu_exports.h"
+#include <stddef.h>
 #include <stdint.h>
-
-/* the switch statement will be optimised away since the compiler already knows
- * the sizeof(type) */
-#define ADDRESS_WRITE(type, addr, val) \
-    { \
-        switch(sizeof(type)) \
-        { \
-            case 4: \
-                uvisor_write32((uint32_t volatile * volatile) (addr), (uint32_t) (val)); \
-                break; \
-            case 2: \
-                uvisor_write16((uint16_t volatile * volatile) (addr), (uint16_t) (val)); \
-                break; \
-            case 1: \
-                uvisor_write8((uint8_t volatile * volatile) (addr), (uint8_t ) (val)); \
-                break; \
-            default: \
-                uvisor_error(USER_NOT_ALLOWED); \
-                break; \
-        } \
-    }
-
-/* the conditional statement will be optimised away since the compiler already
- * knows the sizeof(type) */
-#define ADDRESS_READ(type, addr) \
-    (sizeof(type) == 4 ? uvisor_read32((uint32_t volatile * volatile) (addr)) : \
-     sizeof(type) == 2 ? uvisor_read16((uint16_t volatile * volatile) (addr)) : \
-     sizeof(type) == 1 ? uvisor_read8((uint8_t volatile * volatile) (addr)) : 0)
-
-#define UNION_READ(type, addr, fieldU, fieldB) \
-    ({ \
-        type res; \
-        res.fieldU = ADDRESS_READ(type, addr); \
-        res.fieldB; \
-    })
 
 static UVISOR_FORCEINLINE void uvisor_write32(uint32_t volatile * volatile addr, uint32_t val)
 {
@@ -85,5 +51,41 @@ static UVISOR_FORCEINLINE uint8_t uvisor_read8(uint8_t volatile * volatile addr)
 {
     return UVISOR_ASM_MEMORY_ACCESS(ldrb, uint8_t, addr);
 }
+
+/* The switch statement will be optimised away since the compiler already knows
+ * the sizeof_type. */
+static UVISOR_FORCEINLINE void __address_write(size_t sizeof_type, volatile uint32_t *addr, uint32_t val)
+{
+    switch(sizeof_type) {
+        case 4:
+            uvisor_write32((volatile uint32_t * volatile) addr, (uint32_t) val);
+            break;
+        case 2:
+            uvisor_write16((volatile uint16_t * volatile) addr, (uint16_t) val);
+            break;
+        case 1:
+            uvisor_write8((volatile uint8_t * volatile) addr, (uint8_t) val);
+            break;
+        default:
+            uvisor_error(USER_NOT_ALLOWED);
+            break;
+    }
+}
+
+#define ADDRESS_WRITE(type, addr, val) __address_write(sizeof(type), (volatile uint32_t *) addr, (uint32_t) val)
+
+/* the conditional statement will be optimised away since the compiler already
+ * knows the sizeof(type) */
+#define ADDRESS_READ(type, addr) \
+    (sizeof(type) == 4 ? uvisor_read32((uint32_t volatile * volatile) (addr)) : \
+     sizeof(type) == 2 ? uvisor_read16((uint16_t volatile * volatile) (addr)) : \
+     sizeof(type) == 1 ? uvisor_read8((uint8_t volatile * volatile) (addr)) : 0)
+
+#define UNION_READ(type, addr, fieldU, fieldB) \
+    ({ \
+        type res; \
+        res.fieldU = ADDRESS_READ(type, addr); \
+        res.fieldB; \
+    })
 
 #endif /* __UVISOR_API_SECURE_ACCESS_H__ */
