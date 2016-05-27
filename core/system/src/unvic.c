8898c3d32b46ac76e383781772cd4eb8d7c1feef
Alessandro Angelino <alessandro.angelino@arm.com>
2016-05-27 16:27:45 +0100

    unvic.{c,h}: Fix doxygen comments
    This commit fixes a copy-paste error: s/secure gateway/interrupt.

--- core/system/src/unvic.c
+++ core/system/src/unvic.c
@@ -441,9 +441,8 @@ void UVISOR_NAKED unvic_gateway_in(uint32_t svc_sp, uint32_t svc_pc)
  *
  * @warning This function trusts the SVCall parameters that are passed to it.
  *
- * @param svc_sp[in]    Unprivileged stack pointer at the time of the secure
- *                      gateway
- * @param svc_pc[in]    Program counter at the time of the secure gateway */
+ * @param svc_sp[in]    Unprivileged stack pointer at the time of the interrupt
+ * @param svc_pc[in]    Program counter at the time of the interrupt */
 uint32_t unvic_gateway_context_switch_in(uint32_t svc_sp, uint32_t svc_pc)
 {
     uint8_t dst_id;
@@ -538,8 +537,8 @@ void UVISOR_NAKED unvic_gateway_out(uint32_t svc_sp)
  *
  * @warning This function trusts the SVCall parameters that are passed to it.
  *
- * @param svc_sp[in]    Unprivileged stack pointer at the time of the secure
- *                      gateway return handler (thunk)
+ * @param svc_sp[in]    Unprivileged stack pointer at the time of the interrupt
+ *                      return handler (thunk)
  * @param msp[in]       Value of the MSP register at the time of the SVcall */
 void unvic_gateway_context_switch_out(uint32_t svc_sp, uint32_t msp)
 {
