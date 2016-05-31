aa4430b8a2f5c0253f3802d9eed85ef97992df1a
Alessandro Angelino <alessandro.angelino@arm.com>
2016-05-31 10:15:28 +0100

    vmpu_unpriv_access.h: Add doxygen documentation

--- core/system/inc/mpu/vmpu_unpriv_access.h
+++ core/system/inc/mpu/vmpu_unpriv_access.h
@@ -17,6 +17,17 @@
 #ifndef __VMPU_UNPRIV_ACCESS_H__
 #define __VMPU_UNPRIV_ACCESS_H__
 
+/** Write an 8-bit value to an address as if in unprivileged mode.
+ *
+ * This function can be used even if the MPU is disabled, but the architectural
+ * restrictions on privileged-only registers still apply.
+ *
+ * @warning: Upon failure this function triggers a fault (MemManage if enabled,
+ * otherwise HardFault).
+ *
+ * @param addr[in]  Address to write to.
+ * @param data[in]  8-bit data to write.
+ */
 static UVISOR_FORCEINLINE void vmpu_unpriv_uint8_write(uint32_t addr, uint8_t data)
 {
     asm volatile (
@@ -25,6 +36,17 @@ static UVISOR_FORCEINLINE void vmpu_unpriv_uint8_write(uint32_t addr, uint8_t da
     );
 }
 
+/** Write a 16-bit value to an address as if in unprivileged mode.
+ *
+ * This function can be used even if the MPU is disabled, but the architectural
+ * restrictions on privileged-only registers still apply.
+ *
+ * @warning: Upon failure this function triggers a fault (MemManage if enabled,
+ * otherwise HardFault).
+ *
+ * @param addr[in]  Address to write to.
+ * @param data[in]  16-bit data to write.
+ */
 static UVISOR_FORCEINLINE void vmpu_unpriv_uint16_write(uint32_t addr, uint16_t data)
 {
     asm volatile (
@@ -33,6 +55,17 @@ static UVISOR_FORCEINLINE void vmpu_unpriv_uint16_write(uint32_t addr, uint16_t 
     );
 }
 
+/** Write a 32-bit value to an address as if in unprivileged mode.
+ *
+ * This function can be used even if the MPU is disabled, but the architectural
+ * restrictions on privileged-only registers still apply.
+ *
+ * @warning: Upon failure this function triggers a fault (MemManage if enabled,
+ * otherwise HardFault).
+ *
+ * @param addr[in]  Address to write to.
+ * @param data[in]  32-bit data to write.
+ */
 static UVISOR_FORCEINLINE void vmpu_unpriv_uint32_write(uint32_t addr, uint32_t data)
 {
     asm volatile (
@@ -40,6 +73,17 @@ static UVISOR_FORCEINLINE void vmpu_unpriv_uint32_write(uint32_t addr, uint32_t 
         :: [addr] "r" (addr), [data] "r" (data));
 }
 
+/** Read an 8-bit value from an address as if in unprivileged mode.
+ *
+ * This function can be used even if the MPU is disabled, but the architectural
+ * restrictions on privileged-only registers still apply.
+ *
+ * @warning: Upon failure this function triggers a fault (MemManage if enabled,
+ * otherwise HardFault).
+ *
+ * @param addr[in]  Address to read from.
+ * @returns the 8-bit value read from the address.
+ */
 static UVISOR_FORCEINLINE uint8_t vmpu_unpriv_uint8_read(uint32_t addr)
 {
     uint8_t res;
@@ -50,6 +94,17 @@ static UVISOR_FORCEINLINE uint8_t vmpu_unpriv_uint8_read(uint32_t addr)
     return res;
 }
 
+/** Read a 16-bit value from an address as if in unprivileged mode.
+ *
+ * This function can be used even if the MPU is disabled, but the architectural
+ * restrictions on privileged-only registers still apply.
+ *
+ * @warning: Upon failure this function triggers a fault (MemManage if enabled,
+ * otherwise HardFault).
+ *
+ * @param addr[in]  Address to read from.
+ * @returns the 16-bit value read from the address.
+ */
 static UVISOR_FORCEINLINE uint16_t vmpu_unpriv_uint16_read(uint32_t addr)
 {
     uint16_t res;
@@ -60,6 +115,17 @@ static UVISOR_FORCEINLINE uint16_t vmpu_unpriv_uint16_read(uint32_t addr)
     return res;
 }
 
+/** Read a 32-bit value from an address as if in unprivileged mode.
+ *
+ * This function can be used even if the MPU is disabled, but the architectural
+ * restrictions on privileged-only registers still apply.
+ *
+ * @warning: Upon failure this function triggers a fault (MemManage if enabled,
+ * otherwise HardFault).
+ *
+ * @param addr[in]  Address to read from.
+ * @returns the 32-bit value read from the address.
+ */
 static UVISOR_FORCEINLINE uint32_t vmpu_unpriv_uint32_read(uint32_t addr)
 {
     uint32_t res;
