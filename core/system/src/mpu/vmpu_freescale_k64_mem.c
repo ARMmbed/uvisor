67f56ead3932e183f5c339f5e51946ba4f947d5c
Niklas Hauser <niklas.hauser@arm.com>
2016-06-23 16:40:42 +0100

    Disallow access to page heap

--- core/system/src/mpu/vmpu_freescale_k64_mem.c
+++ core/system/src/mpu/vmpu_freescale_k64_mem.c
@@ -337,7 +337,7 @@ void vmpu_mem_init(void)
 
     /* rest of SRAM, accessible to mbed - non-executable for uvisor */
     res = vmpu_mem_add_int(0, __uvisor_config.bss_end,
-        ((uint32_t) __uvisor_config.sram_end) - ((uint32_t) __uvisor_config.bss_end),
+        UVISOR_REGION_ROUND_UP((uint32_t) __uvisor_config.page_start) - ((uint32_t) __uvisor_config.bss_end),
         UVISOR_TACL_SREAD|
         UVISOR_TACL_SWRITE|
         UVISOR_TACL_UREAD|
