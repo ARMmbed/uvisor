7abb9e44d89e9cfba82ce3001b1c7ce19b258591
Niklas Hauser <niklas.hauser@arm.com>
2016-06-23 16:42:17 +0100

    Allow pushing pages into MPU regions in RR way

--- core/system/inc/mpu/vmpu_freescale_k64_mem.h
+++ core/system/inc/mpu/vmpu_freescale_k64_mem.h
@@ -37,5 +37,6 @@ extern int vmpu_mem_add(uint8_t box_id, void* start, uint32_t size, UvisorBoxAcl
 extern void vmpu_mem_switch(uint8_t src_box, uint8_t dst_box);
 extern uint32_t vmpu_fault_find_acl_mem(uint8_t box_id, uint32_t fault_addr, uint32_t size);
 extern void vmpu_mem_init(void);
+extern int vmpu_mem_push_page_acl(uint32_t start_addr, uint32_t end_addr);
 
 #endif/*__VMPU_FREESCALE_K64_MEM_H__*/
