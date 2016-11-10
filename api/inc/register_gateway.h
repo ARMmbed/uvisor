ARMmbed/uvisor

commit f51198ca378a6add6357f1b3d1a0931924ee851f

Expose all uVisor API via a function table
Instead of calling SVCs in the application directly, the API function in
the uvisor_api table is called, which jumps into uVisors binary and
performs the SVC. This way no synchronization of SVC IDs between uVisor
and the application needs to be performed.

Note that this also speeds up calling the pool queue APIs, since the
position of the export table needs not be manually computed anymore.

api/inc/svc_exports.h
@@ -1,162 +1,0 @@
-/*
- * Copyright (c) 2013-2016, ARM Limited, All Rights Reserved
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#ifndef __UVISOR_API_SVC_EXPORTS_H__
-#define __UVISOR_API_SVC_EXPORTS_H__
-
-#include "api/inc/uvisor_exports.h"
-#include <stdint.h>
-
-/* An SVCall takes a 8bit immediate, which is used as follows:
- *
- * For fast APIs:
- *
- *   7   6   5   4   3   2   1   0
- * .---.---.---.---.---.---.---.---.
- * | 1 | N | N | N | h | h | h | h |
- * '---'---'---'---'---'---'---'---'
- *   |   |_______|   |___________|
- *   |       |         |
- *   |       |         |
- *   |       |         |
- *   |       |        (h) Handler index in the hardcoded table (max 16)
- *   |       |
- *   |      (N) Number of arguments to pass to the handler (max 4)
- *   |
- *   SVCall mode: Fast
- *
- * For slow APIs:
- *
- *   7   6   5   4   3   2   1   0
- * .---.---.---.---.---.---.---.---.
- * | 0 | c | c | c | c | c | c | c |
- * '---'---'---'---'---'---'---'---'
- *   |   |_______________________|
- *   |               |
- *   |               |
- *   |              (c) Handler index in the custom table (max 128)
- *   |
- *   SVCall mode: Slow
- */
-
-/* 1 bit of the SVCall imm8 field is used to determine the mode, fast/slow. */
-#define UVISOR_SVC_MODE_FAST  1
-#define UVISOR_SVC_MODE_SLOW  0
-#define UVISOR_SVC_MODE_BIT   7
-#define UVISOR_SVC_MODE_MASK  ((uint8_t) (1 << UVISOR_SVC_MODE_BIT))
-#define UVISOR_SVC_MODE(mode) ((uint8_t) (((mode) << UVISOR_SVC_MODE_BIT) & UVISOR_SVC_MODE_MASK))
-
-/* 7 or 4 bits of the SVCall imm8 field are used to determine the table index,
- * depending on the mode, fast/slow. */
-#define UVISOR_SVC_FAST_INDEX_MAX  (1 << 4)
-#define UVISOR_SVC_SLOW_INDEX_MAX  (1 << 7)
-#define UVISOR_SVC_FAST_INDEX_BIT  0
-#define UVISOR_SVC_FAST_INDEX_MASK ((uint8_t) (0xF << UVISOR_SVC_FAST_INDEX_BIT))
-#define UVISOR_SVC_SLOW_INDEX_BIT  0
-#define UVISOR_SVC_SLOW_INDEX_MASK ((uint8_t) (0x7F << UVISOR_SVC_SLOW_INDEX_BIT))
-#define UVISOR_SVC_FAST_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_FAST_INDEX_BIT) & UVISOR_SVC_FAST_INDEX_MASK))
-#define UVISOR_SVC_SLOW_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_SLOW_INDEX_BIT) & UVISOR_SVC_SLOW_INDEX_MASK))
-
-/* When the SVC mode is "fast", the imm8 field also contains a specification of
- * the call interface (number of arguments).
- * This is needed for context switches, since the stack manipulation routines
- * need to know how many arguments to copy from source to destination. */
-#define UVISOR_SVC_FAST_NARGS_BIT         4
-#define UVISOR_SVC_FAST_NARGS_MASK        ((uint8_t) (0x7 << UVISOR_SVC_FAST_NARGS_BIT))
-#define UVISOR_SVC_FAST_NARGS_SET(nargs)  ((uint8_t) (((nargs) << UVISOR_SVC_FAST_NARGS_BIT) & UVISOR_SVC_FAST_NARGS_MASK))
-#define UVISOR_SVC_FAST_NARGS_GET(svc_id) (((uint8_t) (svc_id) & UVISOR_SVC_FAST_NARGS_MASK) >> UVISOR_SVC_FAST_NARGS_BIT)
-
-/* Macros to build the SVCall imm8 field.
- * For slow APIs only the SVC handler index is needed.
- * For fast APIs the SVC handler index and the number of arguments are needed. */
-#define UVISOR_SVC_CUSTOM_TABLE(index) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_SLOW) | \
-                                                   UVISOR_SVC_SLOW_INDEX(index)))
-#define UVISOR_SVC_FIXED_TABLE(index, nargs) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_FAST) | \
-                                                         UVISOR_SVC_FAST_INDEX(index) | \
-                                                         UVISOR_SVC_FAST_NARGS_SET(nargs)))
-
-/* SVC immediate values for custom table */
-#define UVISOR_SVC_ID_ISR_SET               UVISOR_SVC_CUSTOM_TABLE(1)
-#define UVISOR_SVC_ID_ISR_GET               UVISOR_SVC_CUSTOM_TABLE(2)
-#define UVISOR_SVC_ID_IRQ_ENABLE            UVISOR_SVC_CUSTOM_TABLE(3)
-#define UVISOR_SVC_ID_IRQ_DISABLE           UVISOR_SVC_CUSTOM_TABLE(4)
-#define UVISOR_SVC_ID_IRQ_PEND_CLR          UVISOR_SVC_CUSTOM_TABLE(5)
-#define UVISOR_SVC_ID_IRQ_PEND_SET          UVISOR_SVC_CUSTOM_TABLE(6)
-#define UVISOR_SVC_ID_IRQ_PEND_GET          UVISOR_SVC_CUSTOM_TABLE(7)
-#define UVISOR_SVC_ID_IRQ_PRIO_SET          UVISOR_SVC_CUSTOM_TABLE(8)
-#define UVISOR_SVC_ID_IRQ_PRIO_GET          UVISOR_SVC_CUSTOM_TABLE(9)
-#define UVISOR_SVC_ID_HALT_USER_ERR         UVISOR_SVC_CUSTOM_TABLE(13)
-#define UVISOR_SVC_ID_IRQ_LEVEL_GET         UVISOR_SVC_CUSTOM_TABLE(14)
-#define UVISOR_SVC_ID_BOX_ID_SELF           UVISOR_SVC_CUSTOM_TABLE(15)
-#define UVISOR_SVC_ID_BOX_ID_CALLER         UVISOR_SVC_CUSTOM_TABLE(16)
-#define UVISOR_SVC_ID_BOX_NAMESPACE_FROM_ID UVISOR_SVC_CUSTOM_TABLE(17)
-#define UVISOR_SVC_ID_DEBUG_REBOOT          UVISOR_SVC_CUSTOM_TABLE(18)
-#define UVISOR_SVC_ID_DEBUG_REGISTER_BOX    UVISOR_SVC_CUSTOM_TABLE(19)
-#define UVISOR_SVC_ID_IRQ_DISABLE_ALL       UVISOR_SVC_CUSTOM_TABLE(20)
-#define UVISOR_SVC_ID_IRQ_ENABLE_ALL        UVISOR_SVC_CUSTOM_TABLE(21)
-#define UVISOR_SVC_ID_PAGE_MALLOC           UVISOR_SVC_CUSTOM_TABLE(22)
-#define UVISOR_SVC_ID_PAGE_FREE             UVISOR_SVC_CUSTOM_TABLE(23)
-
-/* SVC immediate values for hardcoded table (call from unprivileged) */
-#define UVISOR_SVC_ID_UNVIC_OUT           UVISOR_SVC_FIXED_TABLE(0, 0)
-/* Deprecated: UVISOR_SVC_ID_CX_IN(nargs) UVISOR_SVC_FIXED_TABLE(1, nargs) */
-/* Deprecated: UVISOR_SVC_ID_CX_OUT       UVISOR_SVC_FIXED_TABLE(2, 0) */
-#define UVISOR_SVC_ID_REGISTER_GATEWAY    UVISOR_SVC_FIXED_TABLE(3, 0)
-#define UVISOR_SVC_ID_BOX_INIT_FIRST      UVISOR_SVC_FIXED_TABLE(4, 0)
-#define UVISOR_SVC_ID_BOX_INIT_NEXT       UVISOR_SVC_FIXED_TABLE(5, 0)
-
-/* SVC immediate values for hardcoded table (call from privileged) */
-#define UVISOR_SVC_ID_UNVIC_IN         UVISOR_SVC_FIXED_TABLE(0, 0)
-
-/** Generate the SVCall opcode from the SVC ID. */
-#define UVISOR_SVC_OPCODE(id) ((uint16_t) 0xDF00 | (uint8_t) ((id) & 0xFF))
-
-/* macro to execute an SVCall; additional metadata can be provided, which will
- * be appended right after the svc instruction */
-/* note: the macro is implicitly overloaded to allow 0 to 4 32bits arguments */
-#if defined(__CC_ARM)
-
-#elif defined(__GNUC__)
-
-#define UVISOR_SVC(id, metadata, ...) \
-    ({ \
-        UVISOR_MACRO_REGS_ARGS(uint32_t, ##__VA_ARGS__); \
-        UVISOR_MACRO_REGS_RETVAL(uint32_t, res); \
-        asm volatile( \
-            "svc %[svc_id]\n" \
-            metadata \
-            : UVISOR_MACRO_GCC_ASM_OUTPUT(res) \
-            : UVISOR_MACRO_GCC_ASM_INPUT(__VA_ARGS__), \
-              [svc_id] "I" ((id) & 0xFF) \
-        ); \
-        res; \
-    })
-
-#define UVISOR_FUNCTION_CALL(dst_fn, ...) \
-    ({ \
-        UVISOR_MACRO_REGS_ARGS(uint32_t, ##__VA_ARGS__); \
-        UVISOR_MACRO_REGS_RETVAL(uint32_t, res); \
-        asm volatile( \
-            "bl " UVISOR_TO_STRING(dst_fn) "\n" \
-            : UVISOR_MACRO_GCC_ASM_OUTPUT(res) \
-            : UVISOR_MACRO_GCC_ASM_INPUT(__VA_ARGS__) \
-        ); \
-        res; \
-    })
-
-#endif /* defined(__CC_ARM) || defined(__GNUC__) */
-
-#endif /* __UVISOR_API_SVC_EXPORTS_H__ */

core/system/inc/svc.h
@@ -17,7 +17,140 @@
 #ifndef __SVC_H__
 #define __SVC_H__
 
-#include "api/inc/svc_exports.h"
+#include "api/inc/api.h"
+
+/* An SVCall takes a 8bit immediate, which is used as follows:
+ *
+ * For fast APIs:
+ *
+ *   7   6   5   4   3   2   1   0
+ * .---.---.---.---.---.---.---.---.
+ * | 1 | N | N | N | h | h | h | h |
+ * '---'---'---'---'---'---'---'---'
+ *   |   |_______|   |___________|
+ *   |       |         |
+ *   |       |         |
+ *   |       |         |
+ *   |       |        (h) Handler index in the hardcoded table (max 16)
+ *   |       |
+ *   |      (N) Number of arguments to pass to the handler (max 4)
+ *   |
+ *   SVCall mode: Fast
+ *
+ * For slow APIs:
+ *
+ *   7   6   5   4   3   2   1   0
+ * .---.---.---.---.---.---.---.---.
+ * | 0 | c | c | c | c | c | c | c |
+ * '---'---'---'---'---'---'---'---'
+ *   |   |_______________________|
+ *   |               |
+ *   |               |
+ *   |              (c) Handler index in the custom table (max 128)
+ *   |
+ *   SVCall mode: Slow
+ */
+
+/* 1 bit of the SVCall imm8 field is used to determine the mode, fast/slow. */
+#define UVISOR_SVC_MODE_FAST  1
+#define UVISOR_SVC_MODE_SLOW  0
+#define UVISOR_SVC_MODE_BIT   7
+#define UVISOR_SVC_MODE_MASK  ((uint8_t) (1 << UVISOR_SVC_MODE_BIT))
+#define UVISOR_SVC_MODE(mode) ((uint8_t) (((mode) << UVISOR_SVC_MODE_BIT) & UVISOR_SVC_MODE_MASK))
+
+/* 7 or 4 bits of the SVCall imm8 field are used to determine the table index,
+ * depending on the mode, fast/slow. */
+#define UVISOR_SVC_FAST_INDEX_MAX  (1 << 4)
+#define UVISOR_SVC_SLOW_INDEX_MAX  (1 << 7)
+#define UVISOR_SVC_FAST_INDEX_BIT  0
+#define UVISOR_SVC_FAST_INDEX_MASK ((uint8_t) (0xF << UVISOR_SVC_FAST_INDEX_BIT))
+#define UVISOR_SVC_SLOW_INDEX_BIT  0
+#define UVISOR_SVC_SLOW_INDEX_MASK ((uint8_t) (0x7F << UVISOR_SVC_SLOW_INDEX_BIT))
+#define UVISOR_SVC_FAST_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_FAST_INDEX_BIT) & UVISOR_SVC_FAST_INDEX_MASK))
+#define UVISOR_SVC_SLOW_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_SLOW_INDEX_BIT) & UVISOR_SVC_SLOW_INDEX_MASK))
+
+/* When the SVC mode is "fast", the imm8 field also contains a specification of
+ * the call interface (number of arguments).
+ * This is needed for context switches, since the stack manipulation routines
+ * need to know how many arguments to copy from source to destination. */
+#define UVISOR_SVC_FAST_NARGS_BIT         4
+#define UVISOR_SVC_FAST_NARGS_MASK        ((uint8_t) (0x7 << UVISOR_SVC_FAST_NARGS_BIT))
+#define UVISOR_SVC_FAST_NARGS_SET(nargs)  ((uint8_t) (((nargs) << UVISOR_SVC_FAST_NARGS_BIT) & UVISOR_SVC_FAST_NARGS_MASK))
+#define UVISOR_SVC_FAST_NARGS_GET(svc_id) (((uint8_t) (svc_id) & UVISOR_SVC_FAST_NARGS_MASK) >> UVISOR_SVC_FAST_NARGS_BIT)
+
+/* Macros to build the SVCall imm8 field.
+ * For slow APIs only the SVC handler index is needed.
+ * For fast APIs the SVC handler index and the number of arguments are needed. */
+#define UVISOR_SVC_CUSTOM_TABLE(index) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_SLOW) | \
+                                                   UVISOR_SVC_SLOW_INDEX(index)))
+#define UVISOR_SVC_FIXED_TABLE(index, nargs) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_FAST) | \
+                                                         UVISOR_SVC_FAST_INDEX(index) | \
+                                                         UVISOR_SVC_FAST_NARGS_SET(nargs)))
+
+/* SVC immediate values for custom table */
+#define UVISOR_SVC_ID_ISR_SET               UVISOR_SVC_CUSTOM_TABLE(1)
+#define UVISOR_SVC_ID_ISR_GET               UVISOR_SVC_CUSTOM_TABLE(2)
+#define UVISOR_SVC_ID_IRQ_ENABLE            UVISOR_SVC_CUSTOM_TABLE(3)
+#define UVISOR_SVC_ID_IRQ_DISABLE           UVISOR_SVC_CUSTOM_TABLE(4)
+#define UVISOR_SVC_ID_IRQ_PEND_CLR          UVISOR_SVC_CUSTOM_TABLE(5)
+#define UVISOR_SVC_ID_IRQ_PEND_SET          UVISOR_SVC_CUSTOM_TABLE(6)
+#define UVISOR_SVC_ID_IRQ_PEND_GET          UVISOR_SVC_CUSTOM_TABLE(7)
+#define UVISOR_SVC_ID_IRQ_PRIO_SET          UVISOR_SVC_CUSTOM_TABLE(8)
+#define UVISOR_SVC_ID_IRQ_PRIO_GET          UVISOR_SVC_CUSTOM_TABLE(9)
+#define UVISOR_SVC_ID_HALT_USER_ERR         UVISOR_SVC_CUSTOM_TABLE(13)
+#define UVISOR_SVC_ID_IRQ_LEVEL_GET         UVISOR_SVC_CUSTOM_TABLE(14)
+#define UVISOR_SVC_ID_BOX_ID_SELF           UVISOR_SVC_CUSTOM_TABLE(15)
+#define UVISOR_SVC_ID_BOX_ID_CALLER         UVISOR_SVC_CUSTOM_TABLE(16)
+#define UVISOR_SVC_ID_BOX_NAMESPACE_FROM_ID UVISOR_SVC_CUSTOM_TABLE(17)
+#define UVISOR_SVC_ID_DEBUG_REBOOT          UVISOR_SVC_CUSTOM_TABLE(18)
+#define UVISOR_SVC_ID_DEBUG_REGISTER_BOX    UVISOR_SVC_CUSTOM_TABLE(19)
+#define UVISOR_SVC_ID_IRQ_DISABLE_ALL       UVISOR_SVC_CUSTOM_TABLE(20)
+#define UVISOR_SVC_ID_IRQ_ENABLE_ALL        UVISOR_SVC_CUSTOM_TABLE(21)
+#define UVISOR_SVC_ID_PAGE_MALLOC           UVISOR_SVC_CUSTOM_TABLE(22)
+#define UVISOR_SVC_ID_PAGE_FREE             UVISOR_SVC_CUSTOM_TABLE(23)
+
+/* SVC immediate values for hardcoded table (call from unprivileged) */
+#define UVISOR_SVC_ID_UNVIC_OUT           UVISOR_SVC_FIXED_TABLE(0, 0)
+/* Deprecated: UVISOR_SVC_ID_CX_IN(nargs) UVISOR_SVC_FIXED_TABLE(1, nargs) */
+/* Deprecated: UVISOR_SVC_ID_CX_OUT       UVISOR_SVC_FIXED_TABLE(2, 0) */
+#define UVISOR_SVC_ID_REGISTER_GATEWAY    UVISOR_SVC_FIXED_TABLE(3, 0)
+#define UVISOR_SVC_ID_BOX_INIT_FIRST      UVISOR_SVC_FIXED_TABLE(4, 0)
+#define UVISOR_SVC_ID_BOX_INIT_NEXT       UVISOR_SVC_FIXED_TABLE(5, 0)
+
+/* SVC immediate values for hardcoded table (call from privileged) */
+#define UVISOR_SVC_ID_UNVIC_IN         UVISOR_SVC_FIXED_TABLE(0, 0)
+
+/** Generate the SVCall opcode from the SVC ID. */
+#define UVISOR_SVC_OPCODE(id) ((uint16_t) 0xDF00 | (uint8_t) ((id) & 0xFF))
+
+/* macro to execute an SVCall; additional metadata can be provided, which will
+ * be appended right after the svc instruction */
+/* note: the macro is implicitly overloaded to allow 0 to 4 32bits arguments */
+#define UVISOR_SVC(id, metadata, ...) \
+    ({ \
+        UVISOR_MACRO_REGS_ARGS(uint32_t, ##__VA_ARGS__); \
+        UVISOR_MACRO_REGS_RETVAL(uint32_t, res); \
+        asm volatile( \
+            "svc %[svc_id]\n" \
+            metadata \
+            : UVISOR_MACRO_GCC_ASM_OUTPUT(res) \
+            : UVISOR_MACRO_GCC_ASM_INPUT(__VA_ARGS__), \
+              [svc_id] "I" ((id) & 0xFF) \
+        ); \
+        res; \
+    })
+
+#define UVISOR_FUNCTION_CALL(dst_fn, ...) \
+    ({ \
+        UVISOR_MACRO_REGS_ARGS(uint32_t, ##__VA_ARGS__); \
+        UVISOR_MACRO_REGS_RETVAL(uint32_t, res); \
+        asm volatile( \
+            "bl " UVISOR_TO_STRING(dst_fn) "\n" \
+            : UVISOR_MACRO_GCC_ASM_OUTPUT(res) \
+            : UVISOR_MACRO_GCC_ASM_INPUT(__VA_ARGS__) \
+        ); \
+        res; \
+    })
 
 void svc_init(void);
 void __svc_not_implemented(void);

api/inc/api.h
@@ -1,0 +1,83 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __UVISOR_API_H__
+#define __UVISOR_API_H__
+
+#include "rt_OsEventObserver.h"
+#include "api/inc/uvisor_exports.h"
+#include "api/inc/unvic_exports.h"
+#include "api/inc/debug_exports.h"
+#include "api/inc/halt_exports.h"
+#include "api/inc/pool_queue_exports.h"
+#include "api/inc/page_allocator_exports.h"
+#include <stdint.h>
+
+#define UVISOR_API_MAGIC 0x5C9411B4
+#define UVISOR_API_VERSION (10)
+
+UVISOR_EXTERN_C_BEGIN
+
+typedef struct {
+    uint32_t magic;
+    uint32_t (*get_version)(uint32_t);
+
+    void (*init)(void);
+
+    void     (*irq_enable)(uint32_t irqn);
+    void     (*irq_disable)(uint32_t irqn);
+    void     (*irq_disable_all)(void);
+    void     (*irq_enable_all)(void);
+    void     (*irq_set_vector)(uint32_t irqn, uint32_t vector);
+    uint32_t (*irq_get_vector)(uint32_t irqn);
+    void     (*irq_set_priority)(uint32_t irqn, uint32_t priority);
+    uint32_t (*irq_get_priority)(uint32_t irqn);
+    void     (*irq_set_pending)(uint32_t irqn);
+    uint32_t (*irq_get_pending)(uint32_t irqn);
+    void     (*irq_clear_pending)(uint32_t irqn);
+    int      (*irq_get_level)(void);
+    void     (*irq_system_reset)(TResetReason reason);
+
+    int (*page_malloc)(UvisorPageTable * const table);
+    int (*page_free)(const UvisorPageTable * const table);
+
+    int (*box_namespace)(int box_id, char *box_namespace, size_t length);
+
+    void (*debug_init)(const TUvisorDebugDriver * const driver);
+    void (*error)(THaltUserError reason);
+
+    int                (*pool_init)(uvisor_pool_t *, void *, size_t, size_t, int);
+    int                (*pool_queue_init)(uvisor_pool_queue_t *, uvisor_pool_t *, void *, size_t, size_t, int);
+    uvisor_pool_slot_t (*pool_allocate)(uvisor_pool_t *, uint32_t);
+    void               (*pool_queue_enqueue)(uvisor_pool_queue_t *, uvisor_pool_slot_t);
+    uvisor_pool_slot_t (*pool_free)(uvisor_pool_t *, uvisor_pool_slot_t);
+    uvisor_pool_slot_t (*pool_queue_dequeue)(uvisor_pool_queue_t *, uvisor_pool_slot_t);
+    uvisor_pool_slot_t (*pool_queue_dequeue_first)(uvisor_pool_queue_t *);
+    uvisor_pool_slot_t (*pool_queue_find_first)(uvisor_pool_queue_t *, TQueryFN_Ptr, void *);
+
+    OsEventObserver os_event_observer;
+} UVISOR_PACKED UvisorApi;
+
+extern UvisorApi uvisor_api;
+
+static UVISOR_FORCEINLINE uint32_t uvisor_get_version(void)
+{
+    return uvisor_api.get_version(UVISOR_API_VERSION);
+}
+
+UVISOR_EXTERN_C_END
+
+#endif /* __UVISOR_API_H__ */

api/inc/box_config.h
@@ -157,13 +157,4 @@ UVISOR_EXTERN void const * const main_cfg_ptr;
 
 #define uvisor_ctx (*__uvisor_ps)
 
-/* Copy the box namespace of the specified box ID to the memory provided by
- * box_namespace. The box_namespace's length must be at least
- * MAX_BOX_NAMESPACE_LENGTH bytes. Return how many bytes were copied into
- * box_namespace. Return UVISOR_ERROR_INVALID_BOX_ID if the provided box ID is
- * invalid. Return UVISOR_ERROR_BUFFER_TOO_SMALL if the provided box_namespace
- * is too small to hold MAX_BOX_NAMESPACE_LENGTH bytes. Return
- * UVISOR_ERROR_BOX_NAMESPACE_ANONYMOUS if the box is anonymous. */
-UVISOR_EXTERN int uvisor_box_namespace(int box_id, char *box_namespace, size_t length);
-
 #endif /* __UVISOR_API_BOX_CONFIG_H__ */

commit a7ce44c62932920c7df56dd610b1b7005c7f7163

svc: Make svc_exports.h great again
Removing svc_exports.h broke the register-level gateway. Re-create
svc_exports.h with just the minimum needed to make the register-level
gateway work again.

Fixes: f51198ca378a ("Expose all uVisor API via a function table")

api/inc/register_gateway.h
@@ -19,12 +19,16 @@
 
 #include "api/inc/register_gateway_exports.h"
 #include "api/inc/uvisor_exports.h"
+#include "api/inc/svc_exports.h"
 #include <stdint.h>
 
 /** Get the offset of a struct member.
  * @internal
  */
 #define __UVISOR_OFFSETOF(type, member) ((uint32_t) (&(((type *)(0))->member)))
+
+/** Generate the SVCall opcode from the SVC ID. */
+#define UVISOR_SVC_OPCODE(id) ((uint16_t) (0xDF00 | ((id) & 0xFF)))
 
 /** Generate the opcode of the 16-bit Thumb-2 16-bit T2 encoding of the branch
  *  instruction.

api/inc/svc_exports.h
@@ -1,0 +1,103 @@
+/*
+ * Copyright (c) 2013-2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __UVISOR_API_SVC_EXPORTS_H__
+#define __UVISOR_API_SVC_EXPORTS_H__
+
+#include "api/inc/uvisor_exports.h"
+#include "api/inc/api.h"
+#include <stdint.h>
+
+/* An SVCall takes a 8bit immediate, which is used as follows:
+ *
+ * For fast APIs:
+ *
+ *   7   6   5   4   3   2   1   0
+ * .---.---.---.---.---.---.---.---.
+ * | 1 | N | N | N | h | h | h | h |
+ * '---'---'---'---'---'---'---'---'
+ *   |   |_______|   |___________|
+ *   |       |         |
+ *   |       |         |
+ *   |       |         |
+ *   |       |        (h) Handler index in the hardcoded table (max 16)
+ *   |       |
+ *   |      (N) Number of arguments to pass to the handler (max 4)
+ *   |
+ *   SVCall mode: Fast
+ *
+ * For slow APIs:
+ *
+ *   7   6   5   4   3   2   1   0
+ * .---.---.---.---.---.---.---.---.
+ * | 0 | c | c | c | c | c | c | c |
+ * '---'---'---'---'---'---'---'---'
+ *   |   |_______________________|
+ *   |               |
+ *   |               |
+ *   |              (c) Handler index in the custom table (max 128)
+ *   |
+ *   SVCall mode: Slow
+ */
+
+/* 1 bit of the SVCall imm8 field is used to determine the mode, fast/slow. */
+#define UVISOR_SVC_MODE_FAST  1
+#define UVISOR_SVC_MODE_SLOW  0
+#define UVISOR_SVC_MODE_BIT   7
+#define UVISOR_SVC_MODE_MASK  ((uint8_t) (1 << UVISOR_SVC_MODE_BIT))
+#define UVISOR_SVC_MODE(mode) ((uint8_t) (((mode) << UVISOR_SVC_MODE_BIT) & UVISOR_SVC_MODE_MASK))
+
+/* 7 or 4 bits of the SVCall imm8 field are used to determine the table index,
+ * depending on the mode, fast/slow. */
+#define UVISOR_SVC_FAST_INDEX_MAX  (1 << 4)
+#define UVISOR_SVC_SLOW_INDEX_MAX  (1 << 7)
+#define UVISOR_SVC_FAST_INDEX_BIT  0
+#define UVISOR_SVC_FAST_INDEX_MASK ((uint8_t) (0xF << UVISOR_SVC_FAST_INDEX_BIT))
+#define UVISOR_SVC_SLOW_INDEX_BIT  0
+#define UVISOR_SVC_SLOW_INDEX_MASK ((uint8_t) (0x7F << UVISOR_SVC_SLOW_INDEX_BIT))
+#define UVISOR_SVC_FAST_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_FAST_INDEX_BIT) & UVISOR_SVC_FAST_INDEX_MASK))
+#define UVISOR_SVC_SLOW_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_SLOW_INDEX_BIT) & UVISOR_SVC_SLOW_INDEX_MASK))
+
+/* When the SVC mode is "fast", the imm8 field also contains a specification of
+ * the call interface (number of arguments).
+ * This is needed for context switches, since the stack manipulation routines
+ * need to know how many arguments to copy from source to destination. */
+#define UVISOR_SVC_FAST_NARGS_BIT         4
+#define UVISOR_SVC_FAST_NARGS_MASK        ((uint8_t) (0x7 << UVISOR_SVC_FAST_NARGS_BIT))
+#define UVISOR_SVC_FAST_NARGS_SET(nargs)  ((uint8_t) (((nargs) << UVISOR_SVC_FAST_NARGS_BIT) & UVISOR_SVC_FAST_NARGS_MASK))
+#define UVISOR_SVC_FAST_NARGS_GET(svc_id) (((uint8_t) (svc_id) & UVISOR_SVC_FAST_NARGS_MASK) >> UVISOR_SVC_FAST_NARGS_BIT)
+
+/* Macros to build the SVCall imm8 field.
+ * For slow APIs only the SVC handler index is needed.
+ * For fast APIs the SVC handler index and the number of arguments are needed. */
+#define UVISOR_SVC_CUSTOM_TABLE(index) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_SLOW) | \
+                                                   UVISOR_SVC_SLOW_INDEX(index)))
+#define UVISOR_SVC_FIXED_TABLE(index, nargs) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_FAST) | \
+                                                         UVISOR_SVC_FAST_INDEX(index) | \
+                                                         UVISOR_SVC_FAST_NARGS_SET(nargs)))
+
+/* SVC immediate values for hardcoded table (call from unprivileged) */
+#define UVISOR_SVC_ID_UNVIC_OUT           UVISOR_SVC_FIXED_TABLE(0, 0)
+/* Deprecated: UVISOR_SVC_ID_CX_IN(nargs) UVISOR_SVC_FIXED_TABLE(1, nargs) */
+/* Deprecated: UVISOR_SVC_ID_CX_OUT       UVISOR_SVC_FIXED_TABLE(2, 0) */
+#define UVISOR_SVC_ID_REGISTER_GATEWAY    UVISOR_SVC_FIXED_TABLE(3, 0)
+#define UVISOR_SVC_ID_BOX_INIT_FIRST      UVISOR_SVC_FIXED_TABLE(4, 0)
+#define UVISOR_SVC_ID_BOX_INIT_NEXT       UVISOR_SVC_FIXED_TABLE(5, 0)
+
+/* SVC immediate values for hardcoded table (call from privileged) */
+#define UVISOR_SVC_ID_UNVIC_IN         UVISOR_SVC_FIXED_TABLE(0, 0)
+
+#endif /* __UVISOR_API_SVC_EXPORTS_H__ */

core/system/inc/svc.h
@@ -17,89 +17,7 @@
 #ifndef __SVC_H__
 #define __SVC_H__
 
-#include "api/inc/api.h"
-
-/* An SVCall takes a 8bit immediate, which is used as follows:
- *
- * For fast APIs:
- *
- *   7   6   5   4   3   2   1   0
- * .---.---.---.---.---.---.---.---.
- * | 1 | N | N | N | h | h | h | h |
- * '---'---'---'---'---'---'---'---'
- *   |   |_______|   |___________|
- *   |       |         |
- *   |       |         |
- *   |       |         |
- *   |       |        (h) Handler index in the hardcoded table (max 16)
- *   |       |
- *   |      (N) Number of arguments to pass to the handler (max 4)
- *   |
- *   SVCall mode: Fast
- *
- * For slow APIs:
- *
- *   7   6   5   4   3   2   1   0
- * .---.---.---.---.---.---.---.---.
- * | 0 | c | c | c | c | c | c | c |
- * '---'---'---'---'---'---'---'---'
- *   |   |_______________________|
- *   |               |
- *   |               |
- *   |              (c) Handler index in the custom table (max 128)
- *   |
- *   SVCall mode: Slow
- */
-
-/* 1 bit of the SVCall imm8 field is used to determine the mode, fast/slow. */
-#define UVISOR_SVC_MODE_FAST  1
-#define UVISOR_SVC_MODE_SLOW  0
-#define UVISOR_SVC_MODE_BIT   7
-#define UVISOR_SVC_MODE_MASK  ((uint8_t) (1 << UVISOR_SVC_MODE_BIT))
-#define UVISOR_SVC_MODE(mode) ((uint8_t) (((mode) << UVISOR_SVC_MODE_BIT) & UVISOR_SVC_MODE_MASK))
-
-/* 7 or 4 bits of the SVCall imm8 field are used to determine the table index,
- * depending on the mode, fast/slow. */
-#define UVISOR_SVC_FAST_INDEX_MAX  (1 << 4)
-#define UVISOR_SVC_SLOW_INDEX_MAX  (1 << 7)
-#define UVISOR_SVC_FAST_INDEX_BIT  0
-#define UVISOR_SVC_FAST_INDEX_MASK ((uint8_t) (0xF << UVISOR_SVC_FAST_INDEX_BIT))
-#define UVISOR_SVC_SLOW_INDEX_BIT  0
-#define UVISOR_SVC_SLOW_INDEX_MASK ((uint8_t) (0x7F << UVISOR_SVC_SLOW_INDEX_BIT))
-#define UVISOR_SVC_FAST_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_FAST_INDEX_BIT) & UVISOR_SVC_FAST_INDEX_MASK))
-#define UVISOR_SVC_SLOW_INDEX(index) ((uint8_t) (((index) << UVISOR_SVC_SLOW_INDEX_BIT) & UVISOR_SVC_SLOW_INDEX_MASK))
-
-/* When the SVC mode is "fast", the imm8 field also contains a specification of
- * the call interface (number of arguments).
- * This is needed for context switches, since the stack manipulation routines
- * need to know how many arguments to copy from source to destination. */
-#define UVISOR_SVC_FAST_NARGS_BIT         4
-#define UVISOR_SVC_FAST_NARGS_MASK        ((uint8_t) (0x7 << UVISOR_SVC_FAST_NARGS_BIT))
-#define UVISOR_SVC_FAST_NARGS_SET(nargs)  ((uint8_t) (((nargs) << UVISOR_SVC_FAST_NARGS_BIT) & UVISOR_SVC_FAST_NARGS_MASK))
-#define UVISOR_SVC_FAST_NARGS_GET(svc_id) (((uint8_t) (svc_id) & UVISOR_SVC_FAST_NARGS_MASK) >> UVISOR_SVC_FAST_NARGS_BIT)
-
-/* Macros to build the SVCall imm8 field.
- * For slow APIs only the SVC handler index is needed.
- * For fast APIs the SVC handler index and the number of arguments are needed. */
-#define UVISOR_SVC_CUSTOM_TABLE(index) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_SLOW) | \
-                                                   UVISOR_SVC_SLOW_INDEX(index)))
-#define UVISOR_SVC_FIXED_TABLE(index, nargs) ((uint8_t) (UVISOR_SVC_MODE(UVISOR_SVC_MODE_FAST) | \
-                                                         UVISOR_SVC_FAST_INDEX(index) | \
-                                                         UVISOR_SVC_FAST_NARGS_SET(nargs)))
-
-/* SVC immediate values for hardcoded table (call from unprivileged) */
-#define UVISOR_SVC_ID_UNVIC_OUT           UVISOR_SVC_FIXED_TABLE(0, 0)
-/* Deprecated: UVISOR_SVC_ID_CX_IN(nargs) UVISOR_SVC_FIXED_TABLE(1, nargs) */
-/* Deprecated: UVISOR_SVC_ID_CX_OUT       UVISOR_SVC_FIXED_TABLE(2, 0) */
-#define UVISOR_SVC_ID_REGISTER_GATEWAY    UVISOR_SVC_FIXED_TABLE(3, 0)
-#define UVISOR_SVC_ID_BOX_INIT_FIRST      UVISOR_SVC_FIXED_TABLE(4, 0)
-#define UVISOR_SVC_ID_BOX_INIT_NEXT       UVISOR_SVC_FIXED_TABLE(5, 0)
-
-/* SVC immediate values for hardcoded table (call from privileged) */
-#define UVISOR_SVC_ID_UNVIC_IN         UVISOR_SVC_FIXED_TABLE(0, 0)
-
-/** Generate the SVCall opcode from the SVC ID. */
-#define UVISOR_SVC_OPCODE(id) ((uint16_t) (0xDF00 | ((id) & 0xFF)))
+#include "api/inc/svc_exports.h"
 
 #define UVISOR_SVC_ID_GET(target) UVISOR_SVC_CUSTOM_TABLE(offsetof(UvisorSvcTarget, target) / sizeof(uint32_t))
 