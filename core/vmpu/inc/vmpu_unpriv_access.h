5154e1fc6279a9fc8c15b40e4831630c26931916
Niklas Hauser <niklas.hauser@arm.com>
2016-11-24 13:28:27 +0000

    Implement unprivileged access for v8-M

--- core/vmpu/inc/vmpu_unpriv_access.h
+++ core/vmpu/inc/vmpu_unpriv_access.h
@@ -18,7 +18,20 @@
 #define __VMPU_UNPRIV_ACCESS_H__
 
 #include "api/inc/uvisor_exports.h"
+#include "halt.h"
 #include <stdint.h>
+
+#if defined(ARCH_MPU_ARMv8M)
+
+#define UVISOR_UNPRIV_ACCESS_READ(size)  ((size) | (0UL << 31))
+#define UVISOR_UNPRIV_ACCESS_WRITE(size) ((size) | (1UL << 31))
+
+#define UVISOR_UNPRIV_ACCESS_IS_WRITE(size)  ((size) & (1UL << 31))
+#define UVISOR_UNPRIV_ACCESS_SIZE(size)  ((size) & ~(1UL << 31))
+
+extern uint32_t vmpu_unpriv_access(uint32_t addr, uint32_t size, uint32_t data);
+
+#endif
 
 /** Write an 8-bit value to an address as if in unprivileged mode.
  *
@@ -32,10 +45,14 @@
  */
 static UVISOR_FORCEINLINE void vmpu_unpriv_uint8_write(uint32_t addr, uint8_t data)
 {
+#if defined(ARCH_MPU_ARMv8M)
+    vmpu_unpriv_access(addr, UVISOR_UNPRIV_ACCESS_WRITE(sizeof(uint8_t)), data);
+#else
     asm volatile (
         "strbt %[data], [%[addr]]\n"
         :: [addr] "r" (addr), [data] "r" (data)
     );
+#endif
 }
 
 /** Write a 16-bit value to an address as if in unprivileged mode.
@@ -51,10 +68,14 @@ static UVISOR_FORCEINLINE void vmpu_unpriv_uint8_write(uint32_t addr, uint8_t da
  */
 static UVISOR_FORCEINLINE void vmpu_unpriv_uint16_write(uint32_t addr, uint16_t data)
 {
+#if defined(ARCH_MPU_ARMv8M)
+    vmpu_unpriv_access(addr, UVISOR_UNPRIV_ACCESS_WRITE(sizeof(uint16_t)), data);
+#else
     asm volatile (
         "strht %[data], [%[addr]]\n"
         :: [addr] "r" (addr), [data] "r" (data)
     );
+#endif
 }
 
 /** Write a 32-bit value to an address as if in unprivileged mode.
@@ -70,9 +91,14 @@ static UVISOR_FORCEINLINE void vmpu_unpriv_uint16_write(uint32_t addr, uint16_t 
  */
 static UVISOR_FORCEINLINE void vmpu_unpriv_uint32_write(uint32_t addr, uint32_t data)
 {
+#if defined(ARCH_MPU_ARMv8M)
+    vmpu_unpriv_access(addr, UVISOR_UNPRIV_ACCESS_WRITE(sizeof(uint32_t)), data);
+#else
     asm volatile (
         "strt %[data], [%[addr]]\n"
-        :: [addr] "r" (addr), [data] "r" (data));
+        :: [addr] "r" (addr), [data] "r" (data)
+    );
+#endif
 }
 
 /** Read an 8-bit value from an address as if in unprivileged mode.
@@ -88,12 +114,17 @@ static UVISOR_FORCEINLINE void vmpu_unpriv_uint32_write(uint32_t addr, uint32_t 
  */
 static UVISOR_FORCEINLINE uint8_t vmpu_unpriv_uint8_read(uint32_t addr)
 {
+#if defined(ARCH_MPU_ARMv8M)
+    return (uint8_t) vmpu_unpriv_access(addr, UVISOR_UNPRIV_ACCESS_READ(sizeof(uint8_t)), 0);
+#else
     uint8_t res;
     asm volatile (
         "ldrbt %[res], [%[addr]]\n"
         : [res] "=r" (res)
-        : [addr] "r" (addr));
+        : [addr] "r" (addr)
+    );
     return res;
+#endif
 }
 
 /** Read a 16-bit value from an address as if in unprivileged mode.
@@ -109,12 +140,17 @@ static UVISOR_FORCEINLINE uint8_t vmpu_unpriv_uint8_read(uint32_t addr)
  */
 static UVISOR_FORCEINLINE uint16_t vmpu_unpriv_uint16_read(uint32_t addr)
 {
+#if defined(ARCH_MPU_ARMv8M)
+    return (uint16_t) vmpu_unpriv_access(addr, UVISOR_UNPRIV_ACCESS_READ(sizeof(uint16_t)), 0);
+#else
     uint16_t res;
     asm volatile (
         "ldrht %[res], [%[addr]]\n"
         : [res] "=r" (res)
-        : [addr] "r" (addr));
+        : [addr] "r" (addr)
+    );
     return res;
+#endif
 }
 
 /** Read a 32-bit value from an address as if in unprivileged mode.
@@ -130,6 +166,9 @@ static UVISOR_FORCEINLINE uint16_t vmpu_unpriv_uint16_read(uint32_t addr)
  */
 static UVISOR_FORCEINLINE uint32_t vmpu_unpriv_uint32_read(uint32_t addr)
 {
+#if defined(ARCH_MPU_ARMv8M)
+    return vmpu_unpriv_access(addr, UVISOR_UNPRIV_ACCESS_READ(sizeof(uint32_t)), 0);
+#else
     uint32_t res;
     asm volatile (
         "ldrt %[res], [%[addr]]\n"
@@ -137,6 +176,7 @@ static UVISOR_FORCEINLINE uint32_t vmpu_unpriv_uint32_read(uint32_t addr)
         : [addr] "r" (addr)
     );
     return res;
+#endif
 }
 
 #endif /* __VMPU_UNPRIV_ACCESS_H__ */
