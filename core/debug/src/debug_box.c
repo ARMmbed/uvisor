6fb6f605c76ec117f551151682fd6eb537a8bb5c
Alessandro Angelino <alessandro.angelino@arm.com>
2017-05-05 15:27:24 +0100

    Debug box: Make deprivileging handler core-specific

--- core/debug/src/debug_box.c
+++ core/debug/src/debug_box.c
@@ -46,43 +46,6 @@ void debug_reboot(TResetReason reason)
     NVIC_SystemReset();
 }
 
-/* FIXME: Currently it is not possible to return to a regular execution flow
- *        after the execution of the debug box handler. */
-static void debug_deprivilege_and_return(void * debug_handler, void * return_handler,
-                                         uint32_t a0, uint32_t a1, uint32_t a2, uint32_t a3)
-{
-    /* Source box: Get the current stack pointer. */
-    /* Note: The source stack pointer is only used to assess the stack
-     *       alignment and to read the xpsr. */
-    uint32_t src_sp = context_validate_exc_sf(__get_PSP());
-
-    /* Destination box: The debug box. */
-    uint8_t dst_id = g_debug_box.box_id;
-
-    /* Copy the xPSR from the source exception stack frame. */
-    uint32_t xpsr = vmpu_unpriv_uint32_read((uint32_t) &((uint32_t *) src_sp)[7]);
-
-    /* Destination box: Forge the destination stack frame. */
-    /* Note: We manually have to set the 4 parameters on the destination stack,
-     *       so we will set the API to have nargs=0. */
-    uint32_t dst_sp = context_forge_exc_sf(src_sp, dst_id, (uint32_t) debug_handler, (uint32_t) return_handler, xpsr, 0);
-    ((uint32_t *) dst_sp)[0] = a0;
-    ((uint32_t *) dst_sp)[1] = a1;
-    ((uint32_t *) dst_sp)[2] = a2;
-    ((uint32_t *) dst_sp)[3] = a3;
-
-#if defined(ARCH_CORE_ARMv7M)
-    /* Suspend the OS. */
-    g_priv_sys_hooks.priv_os_suspend();
-#endif /* defined(ARCH_CORE_ARMv7M) */
-
-    context_switch_in(CONTEXT_SWITCH_FUNCTION_DEBUG, dst_id, src_sp, dst_sp);
-
-    /* Upon execution return debug_handler will be executed. Upon return from
-     * debug_handler, return_handler will be executed. */
-    return;
-}
-
 uint32_t debug_get_version(void)
 {
     /* TODO: This function cannot be implemented without a mechanism for
