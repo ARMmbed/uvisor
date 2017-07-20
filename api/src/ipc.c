7c474df49a367b1775e2a96220656e97d6439698
Fangyi Zhou <fangyi.zhou@arm.com>
2017-07-20 15:12:30 +0100

    IPC: Update comment for ipc_free_tokens
    No return value is expected
    tokens_to_free might contain more than 1 token
    Token lock should be acquired and this was not explicitly stated in the
    precondition

--- api/src/ipc.c
+++ api/src/ipc.c
@@ -92,9 +92,9 @@ uint32_t ipc_allocate_token(uint32_t * tokens)
 }
 
 /** Free tokens
+ *  The token lock must have been already acquired
  * @param    tokens[in,out] free the specified tokens_to_free from tokens, atomically modifying tokens
- * @param    tokens_to_free[in] the tokens to free (represented by 1 bits)
- * @return   the token, or 0 if no token available */
+ * @param    tokens_to_free[in] the tokens to free (represented by bits) */
 void ipc_free_tokens(uint32_t * tokens, uint32_t tokens_to_free)
 {
     *tokens &= ~tokens_to_free;
