2a97e8fa8a0ec2ad82ebea5e7a7f9b248c063bbd
Roman Kuznetsov <Roman.Kuznetsov@arm.com>
2017-08-31 00:26:58 -0700

    Don't copy xPSR from the source exception stack frame
    Since we're not switching back to the same point this copying
    makes no sense. Moreover, it can create problematic behavior if
    ICI/IT field is set to non-zero state on the target stack frame
    during the copying.

--- core/debug/src/core_armv7m/debug_box_armv7m.c
+++ core/debug/src/core_armv7m/debug_box_armv7m.c
@@ -41,20 +41,13 @@ void debug_deprivilege_and_die(void * debug_handler, void * return_handler,
     /* Destination box: The debug box. */
     uint8_t dst_id = g_debug_box.box_id;
 
-    /* Copy the xPSR from the source exception stack frame. */
-    uint32_t * xpsr_p = &((uint32_t *) src_sp)[7];
-    uint32_t xpsr = xPSR_T_Msk;
-    if (vmpu_buffer_access_is_ok(g_active_box, xpsr_p, sizeof(*xpsr_p))) {
-        xpsr = vmpu_unpriv_uint32_read((uint32_t) xpsr_p);
-    }
-
     /* FIXME: This makes the debug box overwrite the top of the interrupt stack! */
     g_context_current_states[dst_id].sp = g_debug_interrupt_sp[dst_id];
 
     /* Destination box: Forge the destination stack frame. */
     /* Note: We manually have to set the 4 parameters on the destination stack,
      *       so we will set the API to have nargs=0. */
-    uint32_t dst_sp = context_forge_exc_sf(src_sp, dst_id, (uint32_t) debug_handler, (uint32_t) return_handler, xpsr, 0);
+    uint32_t dst_sp = context_forge_exc_sf(src_sp, dst_id, (uint32_t) debug_handler, (uint32_t) return_handler, xPSR_T_Msk, 0);
     ((uint32_t *) dst_sp)[0] = a0;
     ((uint32_t *) dst_sp)[1] = a1;
     ((uint32_t *) dst_sp)[2] = a2;
